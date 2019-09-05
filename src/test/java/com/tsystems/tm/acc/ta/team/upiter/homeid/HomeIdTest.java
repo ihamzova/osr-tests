bdb62245e9d711d72cd31cd1ccdb33d6e4cf2c86
akrupcha <andrei.krupchak@t-systems.com>
2019-09-05 16:59:25 +0300

    DIGIHUB-34654 - home id tests

--- src/test/java/com/tsystems/tm/acc/ta/team/upiter/homeid/HomeIdTest.java
+++ src/test/java/com/tsystems/tm/acc/ta/team/upiter/homeid/HomeIdTest.java
@@ -1,5 +1,6 @@
 package com.tsystems.tm.acc.ta.team.upiter.homeid;
 
+import com.tsystems.tm.acc.home.id.generator.client.model.PoolHomeId;
 import com.tsystems.tm.acc.home.id.generator.client.model.SingleHomeId;
 import com.tsystems.tm.acc.ta.api.HomeIdGeneratorClient;
 import com.tsystems.tm.acc.ta.apitest.ApiTest;
@@ -8,23 +9,14 @@ import io.qameta.allure.TmsLink;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 import static com.tsystems.tm.acc.ta.api.ResponseSpecBuilders.shouldBeCode;
 import static com.tsystems.tm.acc.ta.api.ResponseSpecBuilders.validatedWith;
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 
 public class HomeIdTest extends ApiTest {
 
     private HomeIdGeneratorClient api;
-
-    private String readFile(Path path, Charset encoding) throws IOException {
-        byte[] encoded = Files.readAllBytes(path);
-        return new String(encoded, encoding);
-    }
 
     @BeforeClass
     public void init() {
@@ -37,5 +29,13 @@ public class HomeIdTest extends ApiTest {
     public void createSingleHomeId()  {
         SingleHomeId response = api.getClient().homeIdGeneratorController().generate().executeAs(validatedWith(shouldBeCode(201)));
         assertNotNull(response);
+    }
+
+    @Test
+    @TmsLink("")
+    @Description("Create 7 Home Ids")
+    public void createPoolOfSevenHomeIds()  {
+        PoolHomeId response = api.getClient().homeIdGeneratorController().generateBatch().numberHomeIdsQuery(7).executeAs(validatedWith(shouldBeCode(201)));
+        assertEquals(response.getHomeIds().size(),7);
     }
 }
