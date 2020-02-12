c96ea3d19d0b71fd401ae806c1745f1bfe0baf28
Lutz Schaefer <Lutz.Schaefer@t-systems.com>
2020-02-12 15:11:17 +0100

    DIGIHUB-28413 OltAutoCommissioningDATGTest and OltAutoCommissioningGFNWTest implemented

--- src/test/java/com/tsystems/tm/acc/ta/team/mercury/commissioning/OltAutoCommissioning.java
+++ src/test/java/com/tsystems/tm/acc/ta/team/mercury/commissioning/OltAutoCommissioning.java
@@ -1,26 +1,40 @@
 package com.tsystems.tm.acc.ta.team.mercury.commissioning;
 
 import com.tsystems.tm.acc.data.models.credentials.Credentials;
+import com.tsystems.tm.acc.data.models.nvt.Nvt;
+import com.tsystems.tm.acc.data.models.oltdevice.OltDevice;
 import com.tsystems.tm.acc.data.osr.models.credentials.CredentialsCase;
+import com.tsystems.tm.acc.data.osr.models.nvt.NvtCase;
+import com.tsystems.tm.acc.olt.resource.inventory.internal.client.model.ANCPSession;
+import com.tsystems.tm.acc.olt.resource.inventory.internal.client.model.Device;
+import com.tsystems.tm.acc.olt.resource.inventory.internal.client.model.UplinkDTO;
 import com.tsystems.tm.acc.ta.api.OltResourceInventoryClient;
 import com.tsystems.tm.acc.ta.data.OsrTestContext;
 import com.tsystems.tm.acc.ta.ui.BaseTest;
+import com.tsystems.tm.acc.ta.ui.pages.oltcommissioning.OltCommissioningPage;
 import com.tsystems.tm.acc.ta.ui.pages.oltcommissioning.OltSearchPage;
 import com.tsystems.tm.acc.ta.util.driver.RHSSOAuthListener;
 import io.qameta.allure.Feature;
 import io.qameta.allure.TmsLink;
 import lombok.extern.slf4j.Slf4j;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 import static com.tsystems.tm.acc.ta.api.ResponseSpecBuilders.shouldBeCode;
 import static com.tsystems.tm.acc.ta.api.ResponseSpecBuilders.validatedWith;
-import static com.tsystems.tm.acc.ta.team.upiter.common.CommonTestData.HTTP_CODE_OK_200;
 
 @Slf4j
 @Feature("Description olt auto-commissioning incl. LC-Commissioning Testcase on Mercury Team-environment")
 @TmsLink("DIGIHUB-XXXXX") // This is the Jira id of a TestSet if applicable
 public class OltAutoCommissioning extends BaseTest {
+
+    private static final Integer HTTP_CODE_OK_200 = 200;
+    private static final Integer TIMEOUT_FOR_OLT_COMMISSIONING = 1 * 60_000;
+    private static final String OLT_DTAG_VPSZ = "49/30/2000/";
+    private static final String OLT_DTAG_FSZ = "76H1";
 
     private OltResourceInventoryClient oltResourceInventoryClient;
 
@@ -31,16 +45,54 @@ public class OltAutoCommissioning extends BaseTest {
 
         OsrTestContext context = OsrTestContext.get();
         Credentials loginData = context.getData().getCredentialsDataProvider().get(CredentialsCase.RHSSOOltResourceInventoryUi);
-        log.debug("loginData user= {}, password = {}", loginData.getLogin(), loginData.getPassword() );
         RHSSOAuthListener.resetLoginData(loginData.getLogin(), loginData.getPassword());
         RHSSOAuthListener.startListening();
-        // clearResourceInventoryDataBase();
+        clearResourceInventoryDataBase();
     }
 
     @Test(description = "DIGIHUB-12345 Test description")
-    public void OltAutoCommissioningTest() {
+    public void OltAutoCommissioningDTAGTest() throws Exception {
         OltSearchPage oltSearchPage = OltSearchPage.openSearchPage();
         oltSearchPage.validateUrl();
+        Thread.sleep(1000);
+        oltSearchPage.searchNotDiscoveredByParameters(getOltDeviceDTAG());
+        Thread.sleep(1000);
+
+        OltCommissioningPage oltCommissioningPage = oltSearchPage.pressAutoCommissionigButton();
+        oltCommissioningPage.validateUrl();
+
+        OsrTestContext context = OsrTestContext.get();
+        Nvt nvt = context.getData().getNvtDataProvider().get(NvtCase.nvtForOltCommissioning);
+        oltCommissioningPage.startOltCommissioning(nvt, TIMEOUT_FOR_OLT_COMMISSIONING);
+
+        String endSz = getOltDeviceDTAG().getVpsz() + getOltDeviceDTAG().getFsz();
+        checkDeviceDTAG(endSz);
+        checkUplink(endSz);
+    }
+
+    /**
+     * check device data from olt-ressource-inventory
+     */
+    private void checkDeviceDTAG(String endsz) {
+        Device device = oltResourceInventoryClient.getClient().deviceInternalController().getOltByEndSZ().
+                endSZQuery(endsz).executeAs(validatedWith(shouldBeCode(HTTP_CODE_OK_200)));
+
+        Assert.assertEquals(device.getEmsNbiName(), "MA5600T");
+        Assert.assertEquals(device.getTkz1(), "02351082");
+        Assert.assertEquals(device.getTkz2(), "02353310");
+        Assert.assertEquals(device.getType(), Device.TypeEnum.OLT);
+    }
+
+    /**
+     * check uplink and ancp-session data from olt-ressource-inventory
+     */
+    private void checkUplink(String endSz) {
+        List<UplinkDTO> uplinkDTOList= oltResourceInventoryClient.getClient().ethernetController().findEthernetLinksByEndsz()
+                .oltEndSzQuery(endSz).executeAs(validatedWith(shouldBeCode(HTTP_CODE_OK_200)));
+
+        Assert.assertEquals(uplinkDTOList.size(), 1L);
+        Assert.assertEquals(uplinkDTOList.get(0).getAncpSessions().size(), 1L );
+        Assert.assertEquals(uplinkDTOList.get(0).getAncpSessions().get(0).getSessionStatus(),  ANCPSession.SessionStatusEnum.ACTIVE);
     }
 
     /**
@@ -49,5 +101,15 @@ public class OltAutoCommissioning extends BaseTest {
     private void clearResourceInventoryDataBase() {
         oltResourceInventoryClient.getClient().automaticallyFillDatabaseController().deleteDatabase()
                 .execute(validatedWith(shouldBeCode(HTTP_CODE_OK_200)));
+    }
+
+    /**
+     * creates OLT with DTAG EndSz for Olt Search Page
+     */
+    public OltDevice getOltDeviceDTAG() {
+        OltDevice oltDevice = new OltDevice();
+        oltDevice.setVpsz(OLT_DTAG_VPSZ);
+        oltDevice.setFsz(OLT_DTAG_FSZ);
+        return oltDevice;
     }
 }
