40637b7517810e2bea95a9d9aafb4257c50e1690
Bela Kovac <bela.kovac@t-systems.com>
2020-02-14 15:44:58 +0100

    DIGIHUB-48967 First simple A4 integration test

---
+++ src/test/java/com/tsystems/tm/acc/ta/team/berlinium/a4ResourceInventoryServiceTest.java
@@ -0,0 +1,32 @@
+package com.tsystems.tm.acc.ta.team.berlinium;
+
+import com.tsystems.tm.acc.ta.robot.osr.A4ResourceInventoryRobot;
+import io.restassured.response.Response;
+import io.qameta.allure.Owner;
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class a4ResourceInventoryServiceTest {
+
+    private A4ResourceInventoryRobot a4ResourceInventoryRobot = new A4ResourceInventoryRobot();
+
+    @Test
+    @Owner("bela.kovac@t-systems.com")
+    public void testCreateNeg_checkLogicalResource_deleteNeg() {
+
+        String uuid = UUID.randomUUID().toString();
+
+        a4ResourceInventoryRobot.createNegWithA4ResourceInventoryApi(uuid);
+        Response response = a4ResourceInventoryRobot.getNegAsLogicalResourceWithA4ResourceInventoryServiceApi(uuid);
+
+        response.then().assertThat()
+                .body("id[0]", equalTo(uuid));
+
+        a4ResourceInventoryRobot.deleteNegWithA4ResourceInventoryApi(uuid);
+
+    }
+
+}
