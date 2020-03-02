c9f73b5187b9fcc6d2c4c5037a0b781a174023ed
Aleksei Miachev <aleksei.miachev@t-systems.com>
2020-03-02 15:03:23 +0100

    Resolve DIGIHUB-53718

--- src/main/java/com/tsystems/tm/acc/ta/pages/osr/oltmaintenance/SelenideExceptions/ErrorMessageFound.java
+++ src/main/java/com/tsystems/tm/acc/ta/pages/osr/oltmaintenance/SelenideExceptions/ErrorMessageFound.java
@@ -7,8 +7,4 @@ public class ErrorMessageFound extends UIAssertionError {
     public ErrorMessageFound(String errorMessage) {
         super(WebDriverRunner.driver(), "Error message found: " + errorMessage);
     }
-
-    public String toString() {
-        return this.getClass().getSimpleName() + ' ' + this.getMessage() + this.uiDetails();
-    }
 }
