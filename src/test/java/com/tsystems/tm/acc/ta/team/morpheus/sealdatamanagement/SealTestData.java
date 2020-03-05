314f55f4495a8330b4651fc8c9687c9df55259ce
Miachev Aleksei <Aleksei.Miachev@t-systems.ru>
2020-03-05 20:40:42 +0300

    DIGIHUB-54435

--- src/test/java/com/tsystems/tm/acc/ta/team/morpheus/sealdatamanagement/SealTestData.java
+++ src/test/java/com/tsystems/tm/acc/ta/team/morpheus/sealdatamanagement/SealTestData.java
@@ -1,15 +1,13 @@
 package com.tsystems.tm.acc.ta.team.morpheus.sealdatamanagement;
 
 import com.tsystems.tm.acc.ta.robot.osr.WiremockRobot;
-import org.junit.Ignore;
-import org.junit.Test;
+import org.testng.annotations.Test;
 
 public class SealTestData {
 
     private WiremockRobot wiremockRobot = new WiremockRobot();
 
-    @Test
-    @Ignore
+    @Test(enabled = false)
     public void TestMock() {
 
         wiremockRobot.initializeWiremock("/team.morpheus/wiremock");
