ebdd7914434826b4b36307c52c9a37007609c72f
Stefan Masztalerz <stefan.masztalerz@aoe.com>
2020-03-24 17:51:02 +0100

    DIGIHUB-0 remove use of static json for negCreation

--- src/test/java/com/tsystems/tm/acc/ta/team/berlinium/A4ResourceInventoryServiceTest.java
+++ src/test/java/com/tsystems/tm/acc/ta/team/berlinium/A4ResourceInventoryServiceTest.java
@@ -1,10 +1,12 @@
 package com.tsystems.tm.acc.ta.team.berlinium;
 
 import com.tsystems.tm.acc.ta.robot.osr.A4ResourceInventoryRobot;
+import com.tsystems.tm.acc.tests.osr.a4.resource.inventory.internal.client.model.NetworkElementGroupDto;
 import io.restassured.response.Response;
 import io.qameta.allure.Owner;
 import org.testng.annotations.Test;
 
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -18,8 +20,19 @@ public class A4ResourceInventoryServiceTest {
     public void testCreateNeg_checkLogicalResource_deleteNeg() {
 
         String uuid = UUID.randomUUID().toString();
+        NetworkElementGroupDto networkElementGroup = new NetworkElementGroupDto()
+                .uuid(uuid)
+                .type("POD")
+                .specificationVersion("1")
+                .operationalState("INSTALLING")
+                .name(UUID.randomUUID().toString().substring(0, 6))
+                .lifeCycleState("UNINSTALLING")
+                .lastUpdateTime(OffsetDateTime.now())
+                .description("NEG created during osr-test integration test")
+                .creationTime(OffsetDateTime.now())
+                .centralOfficeNetworkOperator("neg_centOffNetOp_for_integration_test");
 
-        a4ResourceInventoryRobot.createNegWithA4ResourceInventoryApi(uuid);
+        a4ResourceInventoryRobot.createNetworkElementGroup(networkElementGroup);
         Response response = a4ResourceInventoryRobot.getNegAsLogicalResourceWithA4ResourceInventoryServiceApi(uuid);
 
         response.then().assertThat()
