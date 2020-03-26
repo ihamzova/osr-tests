22c4125ee51e292ec6cdf1af9be1fcbef033d808
Bela Kovac <bela.kovac@t-systems.com>
2020-03-26 17:32:39 +0100

    DIGIHUB-48967 Streamlines robot steps for a4-resource-inventory*

--- src/test/java/com/tsystems/tm/acc/ta/team/berlinium/A4ResourceInventoryServiceTest.java
+++ src/test/java/com/tsystems/tm/acc/ta/team/berlinium/A4ResourceInventoryServiceTest.java
@@ -1,9 +1,10 @@
 package com.tsystems.tm.acc.ta.team.berlinium;
 
 import com.tsystems.tm.acc.ta.robot.osr.A4ResourceInventoryRobot;
+import com.tsystems.tm.acc.ta.robot.osr.A4ResourceInventoryServiceRobot;
 import com.tsystems.tm.acc.tests.osr.a4.resource.inventory.internal.client.model.NetworkElementGroupDto;
-import io.restassured.response.Response;
 import io.qameta.allure.Owner;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import java.time.OffsetDateTime;
@@ -14,31 +15,33 @@ import static org.hamcrest.CoreMatchers.equalTo;
 public class A4ResourceInventoryServiceTest {
 
     private A4ResourceInventoryRobot a4ResourceInventoryRobot = new A4ResourceInventoryRobot();
+    private A4ResourceInventoryServiceRobot a4ResourceInventoryServiceRobot = new A4ResourceInventoryServiceRobot();
 
     @Test
     @Owner("bela.kovac@t-systems.com")
     public void testCreateNeg_checkLogicalResource_deleteNeg() {
-
+        // GIVEN
         String uuid = UUID.randomUUID().toString();
         NetworkElementGroupDto networkElementGroup = new NetworkElementGroupDto()
                 .uuid(uuid)
                 .type("POD")
                 .specificationVersion("1")
                 .operationalState("INSTALLING")
-                .name(UUID.randomUUID().toString().substring(0, 6))
+                .name("NEG-" + uuid.substring(0, 6))
                 .lifeCycleState("UNINSTALLING")
                 .lastUpdateTime(OffsetDateTime.now())
                 .description("NEG created during osr-test integration test")
                 .creationTime(OffsetDateTime.now())
                 .centralOfficeNetworkOperator("neg_centOffNetOp_for_integration_test");
 
+        // WHEN
         a4ResourceInventoryRobot.createNetworkElementGroup(networkElementGroup);
-        Response response = a4ResourceInventoryRobot.getNegAsLogicalResourceWithA4ResourceInventoryServiceApi(uuid);
 
-        response.then().assertThat()
-                .body("id[0]", equalTo(uuid));
+        // THEN
+        a4ResourceInventoryServiceRobot.checkLogicalResourceIsNetworkElementGroup(networkElementGroup);
 
-        a4ResourceInventoryRobot.deleteNegWithA4ResourceInventoryApi(uuid);
+        // AFTER / Clean-up
+        a4ResourceInventoryRobot.deleteNetworkElementGroup(uuid);
 
     }
 
