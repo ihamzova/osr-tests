bf8faec0563e3bc961c6c29d6d4720d1963df0f7
Stefan Masztalerz <stefan.masztalerz@aoe.com>
2020-04-14 14:47:40 +0200

    Resolve DIGIHUB-50699

---
+++ src/main/java/com/tsystems/tm/acc/ta/pages/osr/a4resourceinventory/A4StartPage.java
@@ -0,0 +1,56 @@
+package com.tsystems.tm.acc.ta.pages.osr.a4resourceinventory;
+
+import com.tsystems.tm.acc.ta.util.OCUrlBuilder;
+import io.qameta.allure.Step;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.By;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.url;
+import static com.tsystems.tm.acc.ta.util.Assert.assertContains;
+
+@Slf4j
+public class A4StartPage {
+
+    public static final String APP = "a4-resource-inventory-ui";
+    public static final String ENDPOINT = "a4-resource-inventory-ui/portal";
+
+    public static final By A4_INVENTORY_IMPORTER_HEADER_LOCATOR = byXpath("//h2[contains(text(),'A4 Resource Inventory Portal')]");
+    public static final By TO_INSTALLATION_BUTTON = By.id("btnInstallation");
+
+    @Step("Validate page")
+    public A4StartPage validate() {
+        $(A4_INVENTORY_IMPORTER_HEADER_LOCATOR).waitUntil(visible, 3000);
+        $(TO_INSTALLATION_BUTTON).waitUntil(visible, 3000);
+        assertContains(url(), ENDPOINT);
+        return this;
+    }
+
+    @Step("Login")
+    public static A4StartPage login(){
+        URL url = new OCUrlBuilder(APP).build();
+        //this part is needed for external users which have problem with the proxy setup...
+        /*URL url = null;
+        try {
+            url = new URL("https://a4-resource-inventory-ui-proxy-berlinium-01.support.magic.telekom.de/");
+            log.info("Opening url " + url.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("broken");
+        }*/
+        return open(url, A4StartPage.class);
+    }
+
+    @Step("Go to Installation")
+    public InstallationPage goToInstallation(){
+        $(TO_INSTALLATION_BUTTON).click();
+        return new InstallationPage();
+    }
+
+}
---
+++ src/test/java/com/tsystems/tm/acc/ta/team/berlinium/A4ResourceInventoryImportTest.java
@@ -0,0 +1,100 @@
+package com.tsystems.tm.acc.ta.team.berlinium;
+
+import com.tsystems.tm.acc.data.models.credentials.Credentials;
+import com.tsystems.tm.acc.data.osr.models.credentials.CredentialsCase;
+import com.tsystems.tm.acc.ta.data.OsrTestContext;
+import com.tsystems.tm.acc.ta.data.generators.A4ResourceInventoryGenerator;
+import com.tsystems.tm.acc.ta.robot.osr.A4ResourceInventoryImportRobot;
+import com.tsystems.tm.acc.ta.robot.osr.A4ResourceInventoryRobot;
+import com.tsystems.tm.acc.ta.ui.BaseTest;
+import com.tsystems.tm.acc.ta.util.driver.RHSSOAuthListener;
+import com.tsytems.tm.acc.domain.osr.csv.A4ResourceInventoryEntry;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.UUID;
+
+@Epic("OS&R") // Domain name
+@Feature("Import Network Element (Group) CSV file into A4 Resource Inventory") // Feature under test
+//@TmsLink("DIGIHUB-0") // Jira id of a TestSet (if applicable)
+public class A4ResourceInventoryImportTest extends BaseTest {
+
+    private A4ResourceInventoryRobot a4ResourceInventoryRobot = new A4ResourceInventoryRobot();
+    private A4ResourceInventoryImportRobot a4ResourceInventoryImportRobot = new A4ResourceInventoryImportRobot();
+    private OsrTestContext context = OsrTestContext.get();
+    private A4ResourceInventoryGenerator a4ResourceInventoryGenerator = new A4ResourceInventoryGenerator();
+
+    @BeforeMethod
+    public void prepareData() {
+        Credentials loginData = context.getData().getCredentialsDataProvider().get(CredentialsCase.RHSSOA4InventoryUi);
+        RHSSOAuthListener.resetLoginData(loginData.getLogin(), loginData.getPassword());
+    }
+
+    @Test(description = "DIGIHUB-0 Import Network Element (Group) CSV file into A4 Resource Inventory")
+    @Owner("bela.kovac@t-systems.com, stefan.masztalerz@aoe.com") // Comma separated owners of this tests. Only required for Domain and higher test levels.
+//    @TmsLink("DIGIHUB-12345") // Jira Id for this test in Xray
+    @Description("Import Network Element (Group) CSV file into A4 Resource Inventory")
+    public void testImportCsvFile() {
+        // Given / Arrange
+        Random random = new Random();
+        String negName = UUID.randomUUID().toString().substring(0, 6);
+        String neVpsz1 = String.format("%d/6151/%s", random.ints(1, 50).findFirst().getAsInt(), random.ints(0, 50).findFirst().getAsInt());
+
+        ArrayList<A4ResourceInventoryEntry> list = new ArrayList<A4ResourceInventoryEntry>();
+        A4ResourceInventoryEntry entry1 = new A4ResourceInventoryEntry()
+                .negCno("operator")
+                .negName(negName)
+                .negDescription("test csv upload via ui group")
+                .neDescription("first NE added via ui")
+                .neFsz("A410")
+                .neLocAddress("Address")
+                .neLocKlsId("123456")
+                .neLocRackId("RackId")
+                .neLocRackPosition("RackPosition")
+                .nePlanningDeviceName("dmst.spine.1")
+                .neVpsz(neVpsz1)
+                .neVsp("DT");
+
+        list.add(entry1);
+
+        String neVpsz2 = String.format("%d/6151/%s", random.ints(1, 50).findFirst().getAsInt(), random.ints(0, 50).findFirst().getAsInt());
+
+        A4ResourceInventoryEntry entry2 = new A4ResourceInventoryEntry()
+                .negCno("operator")
+                .negName(negName)
+                .negDescription("test csv upload via ui group")
+                .neDescription("second NE added via ui")
+                .neFsz("A410")
+                .neLocAddress("Address")
+                .neLocKlsId("123456")
+                .neLocRackId("RackId")
+                .neLocRackPosition("RackPosition")
+                .nePlanningDeviceName("dmst.spine.1")
+                .neVpsz(neVpsz2)
+                .neVsp("DT");
+
+        list.add(entry2);
+
+        File csvFile = Paths.get( "target/","a4Testcase1.csv").toFile();
+        a4ResourceInventoryImportRobot.generateCsv(list, csvFile);
+
+        // When / Action
+        a4ResourceInventoryImportRobot.importCsvFileViaUi(csvFile);
+
+        // Then / Assert
+        a4ResourceInventoryRobot.checkNetworkElementsViaUi(list);
+
+        // After / Clean-up
+        a4ResourceInventoryRobot.deleteNetworkElements(list);
+        a4ResourceInventoryRobot.deleteGroupByName(negName);
+    }
+
+}
