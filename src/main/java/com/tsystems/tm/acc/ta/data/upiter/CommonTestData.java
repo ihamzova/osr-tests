a57e786ec5c6bc3574d7b8f37fef5db05dadd37d
Alina Maiorova <alina.maiorova@t-systems.com>
2020-08-17 14:52:50 +0200

    Resolve DIGIHUB-69049 [TA] [team-env] Automate FTTB pre-provisioning test cases

--- src/main/java/com/tsystems/tm/acc/ta/data/upiter/CommonTestData.java
+++ src/main/java/com/tsystems/tm/acc/ta/data/upiter/CommonTestData.java
@@ -1,8 +1,5 @@
 package com.tsystems.tm.acc.ta.data.upiter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class CommonTestData {
 
     public static final Integer HTTP_CODE_OK_200 = 200;
@@ -14,12 +11,4 @@ public class CommonTestData {
     public static final String STATUS_ACTIVE = "ACTIVE";
     public static final String STATUS_INACTIVE = "INACTIVE";
     public static final String STATUS_WALLED_GARDEN = "WALLED_GARDEN";
-
-    public static List<Integer> calculateExpectedOnuAccessIds(int numberOfAccessLinesForProvisioning) {
-        List<Integer> expectedOnuIdsList = new ArrayList<>();
-        for (int i = 1; i <= numberOfAccessLinesForProvisioning; i++) {
-            expectedOnuIdsList.add(i);
-        }
-        return expectedOnuIdsList;
-    }
 }
