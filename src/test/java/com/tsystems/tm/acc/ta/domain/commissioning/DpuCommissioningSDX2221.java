f71c63bc201e1815a55c1ae89253e1e4f6b94328
azakhare <alina.zakharenko@t-systems.com>
2020-09-07 18:52:37 +0300

    DIGIHUB-69097: new DpuInfoPage was added, DpuCommissioningSDX2221 test was extended with new steps, DpuCreatePage was extended with new step and locator, DpuCommissioningUiRobot.startDpuCommissioning was extended with new steps

--- src/test/java/com/tsystems/tm/acc/ta/domain/commissioning/DpuCommissioningSDX2221.java
+++ src/test/java/com/tsystems/tm/acc/ta/domain/commissioning/DpuCommissioningSDX2221.java
@@ -46,7 +46,7 @@ public class DpuCommissioningSDX2221 extends BaseTest {
     @TmsLink("DIGIHUB-75965")
     @Description("DPU creation and DPU-Commissioning (device : SDX2221-16 TP-AC-MELT) case")
     @Owner("@t-systems.com")
-    public void dpuCommissioning() {
+    public void dpuCommissioning() throws InterruptedException {
         Credentials loginData = context.getData().getCredentialsDataProvider().get(CredentialsCase.RHSSOOltResourceInventoryUiDTAG);
         SelenideConfigurationManager.get().setLoginData(loginData.getLogin(), loginData.getPassword());
 
@@ -86,6 +86,7 @@ public class DpuCommissioningSDX2221 extends BaseTest {
                             "EXECUTED Successfuly [Provision FTTB access provisioning on DPU][callback]",
                             "EXECUTED Successfuly [update LifecycleStatus of DPU to OPERATING]",
                             "EXECUTED Successfuly [update LifecycleStatus of DPU.uplinkPort to OPERATING]"));
+
         }
     }
 }
