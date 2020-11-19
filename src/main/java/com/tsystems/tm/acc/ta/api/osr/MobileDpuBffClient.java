863053d5081b4562dddefe922bac7b0e344f2e65
Anna Dmitrieva <anna.dmitrieva@t-systems.com>
2020-11-19 14:06:27 +0100

    DIGIHUB-85141: added MobileDpuBff client and robot

---
+++ src/main/java/com/tsystems/tm/acc/ta/api/osr/MobileDpuBffClient.java
@@ -0,0 +1,36 @@
+package com.tsystems.tm.acc.ta.api.osr;
+
+import com.tsystems.tm.acc.ta.api.Resetable;
+import com.tsystems.tm.acc.ta.util.OCUrlBuilder;
+import com.tsystems.tm.acc.tests.osr.mobile.dpu.bff.invoker.JSON;
+import com.tsystems.tm.acc.tests.osr.mobile.dpu.bff.invoker.ApiClient;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import lombok.Getter;
+import static com.tsystems.tm.acc.tests.osr.line.id.generator.internal.client.invoker.GsonObjectMapper.gson;
+import static io.restassured.RestAssured.config;
+import static io.restassured.config.ObjectMapperConfig.objectMapperConfig;
+
+public class MobileDpuBffClient implements Resetable {
+
+    @Getter
+    private ApiClient client;
+
+    public MobileDpuBffClient() {
+        client = ApiClient.api(ApiClient.Config.apiConfig().reqSpecSupplier(
+                () -> new RequestSpecBuilder().setConfig(config().objectMapperConfig(objectMapperConfig().defaultObjectMapper(gson())))
+                        .addFilter(new RequestLoggingFilter())
+                        .addFilter(new ResponseLoggingFilter())
+                        .setBaseUri(new OCUrlBuilder("mobile-dpu-bff").buildUri())));
+    }
+
+    public static JSON json() {
+        return new JSON();
+    }
+
+    @Override
+    public void reset() {
+    }
+
+}
---
+++ src/main/java/com/tsystems/tm/acc/ta/robot/osr/MobileDpuBffRobot.java
@@ -0,0 +1,40 @@
+package com.tsystems.tm.acc.ta.robot.osr;
+
+import com.tsystems.tm.acc.ta.api.osr.MobileDpuBffClient;
+import com.tsystems.tm.acc.tests.osr.mobile.dpu.bff.model.WorkorderResponse;
+import io.qameta.allure.Step;
+import org.testng.Assert;
+
+
+import static com.tsystems.tm.acc.ta.api.ResponseSpecBuilders.shouldBeCode;
+import static com.tsystems.tm.acc.ta.api.ResponseSpecBuilders.validatedWith;
+
+public class MobileDpuBffRobot {
+
+    private MobileDpuBffClient mobileDpuBffClient;
+
+    @Step("Returns a WorkorderResponse determined by given Workorder-Id.")
+
+public void getWorkorder (String woid){
+        mobileDpuBffClient = new MobileDpuBffClient();
+        WorkorderResponse workorderResponse = mobileDpuBffClient.getClient().mobileDpuBffInternal().getWorkorder()
+                .woIdPath(woid)
+                .executeAs(validatedWith(shouldBeCode(200)));
+        Assert.assertEquals(workorderResponse.getId().longValue(), 2L);
+        Assert.assertEquals(workorderResponse.getStatus(), "CREATED");
+        Assert.assertEquals(workorderResponse.getFolId().longValue(), 2L);
+        Assert.assertEquals(workorderResponse.getType(), "DPU_INSTALLATION");
+
+    }
+
+    @Step("Starts a workorder for given Workorder-Id and returns WorkorderResponse.")
+    public void startWorkorder(){
+
+    }
+
+    @Step("Completes a workorder for given Workorder-Id and returns WorkorderResponse.")
+    public void completeWorkorder(){
+
+    }
+
+}
