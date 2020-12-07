87d148e4210f7c426fdbb2dbdbe1627e031d4f88
Petr Popov <petr.popov@t-systems.com>
2020-12-07 09:26:21 +0100

    DIGIHUB-85141 : add user auth

--- src/main/java/com/tsystems/tm/acc/ta/api/osr/MobileDpuBffClient.java
+++ src/main/java/com/tsystems/tm/acc/ta/api/osr/MobileDpuBffClient.java
@@ -1,72 +1,51 @@
 package com.tsystems.tm.acc.ta.api.osr;
 
-import com.tsystems.tm.acc.ta.api.*;
+import com.tsystems.tm.acc.ta.api.APMHeaderAuthTokenInjector;
+import com.tsystems.tm.acc.ta.api.AuthTokenProvider;
+import com.tsystems.tm.acc.ta.api.RequestSpecBuilders;
+import com.tsystems.tm.acc.ta.api.Resetable;
+import com.tsystems.tm.acc.ta.helpers.morpheus.UserTokenProvider;
 import com.tsystems.tm.acc.ta.util.OCUrlBuilder;
+import com.tsystems.tm.acc.tests.osr.mobile.dpu.bff.invoker.ApiClient;
 import com.tsystems.tm.acc.tests.osr.mobile.dpu.bff.invoker.GsonObjectMapper;
 import com.tsystems.tm.acc.tests.osr.mobile.dpu.bff.invoker.JSON;
-import com.tsystems.tm.acc.tests.osr.mobile.dpu.bff.invoker.ApiClient;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
-import static com.tsystems.tm.acc.ta.wiremock.ExtendedWireMock.PROVIDER_CLIENT_ID;
-import static com.tsystems.tm.acc.tests.osr.line.id.generator.internal.client.invoker.GsonObjectMapper.gson;
-import static io.restassured.RestAssured.config;
+import static com.tsystems.tm.acc.tests.osr.olt.resource.inventory.external.v4_6_0.client.invoker.GsonObjectMapper.gson;
 import static io.restassured.config.ObjectMapperConfig.objectMapperConfig;
+import static io.restassured.config.RestAssuredConfig.config;
 
+@Slf4j
 public class MobileDpuBffClient implements Resetable {
+	private static final AuthTokenProvider authTokenProvider = new UserTokenProvider(
+			"mobile_dpu_admin", "Mobile!3","mobile-dpu-user");
 
-    @Getter
-    private ApiClient client;
 
-    private static final WiremockRhssoAuthCodeProvider authCodeProvider = new WiremockRhssoAuthCodeProvider();
-    private static final AuthTokenProvider authTokenProvider =  new RhssoBrowserFlowAuthTokenProvider(
-            PROVIDER_CLIENT_ID, authCodeProvider);
-
-/*    private static final WiremockRhssoAuthCodeProvider authCodeProvider = new WiremockRhssoAuthCodeProvider();
-    private static final AuthTokenProvider authTokenProvider =  new CachedRhssoBrowserFlowAuthTokenProvider(
-            PROVIDER_CLIENT_ID, authCodeProvider, new TokenCache("PonInventoryClient"));
-
-    public MobileDpuBffClientAPM() {
-        client = ApiClient.api(ApiClient.Config.apiConfig().reqSpecSupplier(
-                () -> RequestSpecBuilders.getDefaultWithAuth(
-                        GsonObjectMapper.gson(),
-                        new OCUrlBuilder("pon-inventory-incomplete")
-                                .buildUri(),
-                        new APMHeaderAuthTokenInjector(authTokenProvider))
-                        .addFilter(new RequestIdFilter())
-        ));
-    }*/
-
-/*    public MobileDpuBffClient() {
-        client = ApiClient.api(ApiClient.Config.apiConfig().reqSpecSupplier(
-                () -> RequestSpecBuilders.getDefaultWithAuth(
-                        GsonObjectMapper.gson(),
-                        new OCUrlBuilder("mobile-dpu-bff")
-                                .withoutSuffix()
-                                .withoutAuth()
-                                .buildExternalUri(),
-                        new BearerHeaderAuthTokenInjector(authTokenProvider))
-        ));
-    }*/
+	@Getter
+	private ApiClient client;
 
     public MobileDpuBffClient() {
-        client = ApiClient.api(ApiClient.Config.apiConfig().reqSpecSupplier(
-                () -> new RequestSpecBuilder().setConfig(config().objectMapperConfig(objectMapperConfig().defaultObjectMapper(gson())))
-                        .addFilter(new RequestLoggingFilter())
-                        .addFilter(new ResponseLoggingFilter())
-                        .setBaseUri(new OCUrlBuilder("mobile-dpu-bff").buildUri())));
-    }
+		client = ApiClient.api(ApiClient.Config.apiConfig().reqSpecSupplier(
+				() -> RequestSpecBuilders.getDefaultWithAuth(
+						GsonObjectMapper.gson(),
+						new OCUrlBuilder("mobile-dpu-bff-app")
+								.withoutSuffix()
+								.withoutAuth()
+								.buildUri(),
+						new APMHeaderAuthTokenInjector(authTokenProvider))
+		));
+	}
 
 
+	public static JSON json() {
+		return new JSON();
+	}
 
-    public static JSON json() {
-        return new JSON();
-    }
-
-    @Override
-    public void reset() {
-    }
-
+	@Override
+	public void reset() {
+	}
 }
