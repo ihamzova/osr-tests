e160b530913784d9423c7e6239c20b830ec2e333
Alina Maiorova <alina.maiorova@t-systems.com>
2021-02-16 14:11:02 +0100

    Resolve DIGIHUB-91329 updated wg-fttb-access-provisioning swagger

--- src/main/java/com/tsystems/tm/acc/ta/api/osr/WgFttbAccessProvisioningClient.java
+++ src/main/java/com/tsystems/tm/acc/ta/api/osr/WgFttbAccessProvisioningClient.java
@@ -2,13 +2,13 @@ package com.tsystems.tm.acc.ta.api.osr;
 
 import com.tsystems.tm.acc.ta.api.Resetable;
 import com.tsystems.tm.acc.ta.util.OCUrlBuilder;
-import com.tsystems.tm.acc.tests.osr.wg.fttb.access.provisioning.internal.v1.client.invoker.ApiClient;
+import com.tsystems.tm.acc.tests.osr.wg.fttb.access.provisioning.v1_2_0.client.invoker.ApiClient;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import lombok.Getter;
 
-import static com.tsystems.tm.acc.tests.osr.wg.fttb.access.provisioning.internal.v1.client.invoker.GsonObjectMapper.gson;
+import static com.tsystems.tm.acc.tests.osr.wg.fttb.access.provisioning.v1_2_0.client.invoker.GsonObjectMapper.gson;
 import static io.restassured.RestAssured.config;
 import static io.restassured.config.ObjectMapperConfig.objectMapperConfig;
 
