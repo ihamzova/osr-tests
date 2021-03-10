a8b62831a43709adffe955a617250af4dd71cb42
Alina Mayorova <alina.mayorova@mail.ru>
2021-03-10 14:15:38 +0300

    DIGIHUB-58101: added assertions for al-ri

--- src/main/java/com/tsystems/tm/acc/ta/robot/osr/OltDeCommissioningRobot.java
+++ src/main/java/com/tsystems/tm/acc/ta/robot/osr/OltDeCommissioningRobot.java
@@ -10,6 +10,7 @@ import com.tsystems.tm.acc.ta.pages.osr.oltcommissioning.*;
 import com.tsystems.tm.acc.tests.osr.access.line.resource.inventory.v5_8_0.client.model.*;
 import com.tsystems.tm.acc.tests.osr.olt.resource.inventory.internal.client.model.*;
 import io.qameta.allure.Step;
+import org.apache.http.protocol.HTTP;
 import org.testng.Assert;
 
 import java.util.List;
@@ -18,6 +19,7 @@ import java.util.stream.Collectors;
 
 import static com.tsystems.tm.acc.ta.api.ResponseSpecBuilders.shouldBeCode;
 import static com.tsystems.tm.acc.ta.api.ResponseSpecBuilders.validatedWith;
+import static org.testng.Assert.assertEquals;
 
 public class OltDeCommissioningRobot {
     private static final Integer HTTP_CODE_OK_200 = 200;
@@ -28,7 +30,7 @@ public class OltDeCommissioningRobot {
     private static final Integer TIMEOUT_FOR_CARD_DELETION = 5_000;
 
     private OltResourceInventoryClient oltResourceInventoryClient = new OltResourceInventoryClient();
-
+    private AccessLineResourceInventoryClient accessLineResourceInventoryClient = new AccessLineResourceInventoryClient();
 
     @Step("Start olt decommissionung process after manual commissioning")
     public  void startOltDecommissioningAfterManualCommissioning(OltDevice olt) throws InterruptedException {
@@ -42,7 +44,7 @@ public class OltDeCommissioningRobot {
         oltDetailsPage.startAccessLinesDeProvisioningFromCard(TIMEOUT_FOR_CARD_DEPROVISIONING);
         oltDetailsPage.deconfigureAncpSession();
         oltDetailsPage.deleteUplinkConfiguration();
-        Assert.assertEquals(oltDetailsPage.getDeviceLifeCycleState(), DevicePortLifeCycleStateUI.NOTOPERATING.toString());
+        assertEquals(oltDetailsPage.getDeviceLifeCycleState(), DevicePortLifeCycleStateUI.NOTOPERATING.toString());
         oltDetailsPage.deleteGponCard();
         Thread.sleep(TIMEOUT_FOR_CARD_DELETION);
         oltDetailsPage.deleteDevice();
@@ -63,7 +65,7 @@ public class OltDeCommissioningRobot {
         oltDetailsPage.startAccessLinesDeProvisioningFromDevice(TIMEOUT_FOR_CARD_DEPROVISIONING);
         oltDetailsPage.deconfigureAncpSession();
         oltDetailsPage.deleteUplinkConfiguration();
-        Assert.assertEquals(oltDetailsPage.getDeviceLifeCycleState(), DevicePortLifeCycleStateUI.NOTOPERATING.toString());
+        assertEquals(oltDetailsPage.getDeviceLifeCycleState(), DevicePortLifeCycleStateUI.NOTOPERATING.toString());
         oltDetailsPage.deleteGponCard();
         Thread.sleep(TIMEOUT_FOR_CARD_DELETION);
         oltDetailsPage.deleteDevice();
@@ -73,17 +75,46 @@ public class OltDeCommissioningRobot {
         Thread.sleep(TIMEOUT_FOR_DEVICE_DELETION);
     }
 
-    @Step("Checks olt data in olt-ri after decommissioning process")
+    @Step("Checks olt data in olt-ri and al-ri after decommissioning process")
     public void checkOltDeCommissioningResult(OltDevice olt, String slot) {
         String oltEndSz = olt.getVpsz() + "/" + olt.getFsz();
 
         List<Device> deviceList = oltResourceInventoryClient.getClient().deviceInternalController().findDeviceByCriteria()
                 .endszQuery(oltEndSz).executeAs(validatedWith(shouldBeCode(HTTP_CODE_OK_200)));
-        Assert.assertEquals(deviceList.size(), 0L, "Device is present");
+        assertEquals(deviceList.size(), 0L, "Device is present");
 
         oltResourceInventoryClient.getClient().cardController().findCard()
                 .endSzQuery(oltEndSz).slotNumberQuery(slot).executeAs(validatedWith(shouldBeCode(HTTP_CODE_NOT_FOUND_404)));
 
-    }
+        List<AccessLineDto> ftthAccessLines = accessLineResourceInventoryClient.getClient().accessLineController().searchAccessLines()
+                .body(new SearchAccessLineDto().endSz(olt.getEndsz()))
+                .executeAs(validatedWith(shouldBeCode(HTTP_CODE_OK_200)))
+                .stream().collect(Collectors.toList());
 
+        List<HomeIdDto> homeIds = accessLineResourceInventoryClient.getClient().homeIdController().searchHomeIds()
+                .body(new SearchHomeIdDto().endSz(olt.getEndsz()))
+                .executeAs(validatedWith(shouldBeCode(HTTP_CODE_OK_200)))
+                .stream().collect(Collectors.toList());
+
+        List<LineIdDto> lineIds = accessLineResourceInventoryClient.getClient().lineIdController().searchLineIds()
+                .body(new SearchLineIdDto().endSz(olt.getEndsz()))
+                .executeAs(validatedWith(shouldBeCode(HTTP_CODE_OK_200)))
+                .stream().collect(Collectors.toList());
+
+        List<BackhaulIdDto> backhaulIds = accessLineResourceInventoryClient.getClient().backhaulIdController().searchBackhaulIds()
+                .body(new SearchBackhaulIdDto().endSz(olt.getEndsz()))
+                .executeAs(validatedWith(shouldBeCode(HTTP_CODE_OK_200)))
+                .stream().collect(Collectors.toList());
+
+        List<ReferenceDto> physicalResourceRefs = accessLineResourceInventoryClient.getClient().physicalResourceReferenceInternalController().searchPhysicalResourceReference()
+                .body(new SearchPhysicalResourceReferenceDto().endSz(olt.getEndsz()))
+                .executeAs(validatedWith(shouldBeCode(HTTP_CODE_OK_200)))
+                .stream().collect(Collectors.toList());
+
+        assertEquals(ftthAccessLines.size(), 0, "There are AccessLines left");
+        assertEquals(homeIds.size(), 0, "There are HomeIds left");
+        assertEquals(lineIds.size(), 0, "There are LineIds left");
+        assertEquals(backhaulIds.size(), 0, "There are BackhaulIds left");
+        assertEquals(physicalResourceRefs.size(), 0, "There are PhysicalResourceRefs left");
+    }
 }
