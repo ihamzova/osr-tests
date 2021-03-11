05333765571e5920b11d9e873495f0c8dd903791
Anita Junge <anita.junge@t-systems.com>
2021-03-11 11:12:54 +0100

    DIGIHUB-97411 robot for a4-carrier-management-test

--- src/main/java/com/tsystems/tm/acc/ta/robot/osr/A4CarrierManagementRobot.java
+++ src/main/java/com/tsystems/tm/acc/ta/robot/osr/A4CarrierManagementRobot.java
@@ -5,10 +5,12 @@ import com.tsystems.tm.acc.ta.api.osr.A4ResourceInventoryServiceClient;
 import com.tsystems.tm.acc.ta.data.osr.mappers.A4ResourceInventoryServiceMapper;
 import com.tsystems.tm.acc.ta.data.osr.models.*;
 import com.tsystems.tm.acc.tests.osr.a4.carrier.management.client.invoker.ApiClient;
+import com.tsystems.tm.acc.tests.osr.a4.carrier.management.client.model.AllocateL2BsaNspTask;
 import com.tsystems.tm.acc.tests.osr.a4.resource.inventory.service.client.model.LogicalResource;
 import com.tsystems.tm.acc.tests.osr.a4.resource.inventory.service.client.model.LogicalResourceUpdate;
 import com.tsystems.tm.acc.tests.osr.a4.resource.inventory.service.client.model.ResourceCharacteristic;
 import io.qameta.allure.Step;
+import net.minidev.json.JSONObject;
 import org.testng.Assert;
 
 import java.util.List;
@@ -22,8 +24,20 @@ public class A4CarrierManagementRobot {
 
     private final ApiClient a4CarrierManagement = new A4CarrierManagementClient().getClient();
 
-    @Step("allocateL2BsaNspTask")
-    public void allocateL2BsaNsp(A4NetworkServiceProfileA10Nsp nspData) {
+    @Step("send POST for allocateL2BsaNspTask")
+    public void sendPostForAllocateL2BsaNsp
+            (String lineId, String carrierBsaReference, int dataRateUp, int dataRateDown) {
+        AllocateL2BsaNspTask allocateL2BsaNspTask = new AllocateL2BsaNspTask();
+        allocateL2BsaNspTask.setLineId(lineId);
+        allocateL2BsaNspTask.setCarrierBsaReference(carrierBsaReference);
+        allocateL2BsaNspTask.setDataRateDown(dataRateDown);
+        allocateL2BsaNspTask.setDataRateUp(dataRateUp);
+
+        a4CarrierManagement
+                .allocateL2BsaNspTask()
+                .allocateL2BsaNspTask()
+                .body(allocateL2BsaNspTask)
+                .execute(validatedWith(shouldBeCode(HTTP_CODE_OK_200)));
 
     }
 
