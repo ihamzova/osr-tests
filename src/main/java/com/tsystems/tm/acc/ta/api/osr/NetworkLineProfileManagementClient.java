7b9da6234312afe9999bf2278105b29baeee0698
erymar <Elena.Rymar@t-systems.com>
2021-03-15 15:04:59 +0300

    DIGIHUB-91389 change nlpm client

--- src/main/java/com/tsystems/tm/acc/ta/api/osr/NetworkLineProfileManagementClient.java
+++ src/main/java/com/tsystems/tm/acc/ta/api/osr/NetworkLineProfileManagementClient.java
@@ -2,14 +2,14 @@ package com.tsystems.tm.acc.ta.api.osr;
 
 import com.tsystems.tm.acc.ta.api.Resetable;
 import com.tsystems.tm.acc.ta.util.OCUrlBuilder;
-import com.tsystems.tm.acc.tests.osr.network.line.profile.management.internal.v1_4_0.client.invoker.ApiClient;
-import com.tsystems.tm.acc.tests.osr.network.line.profile.management.internal.v1_4_0.client.invoker.JSON;
+import com.tsystems.tm.acc.tests.osr.network.line.profile.management.v1_5_0.client.invoker.ApiClient;
+import com.tsystems.tm.acc.tests.osr.network.line.profile.management.v1_5_0.client.invoker.JSON;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import lombok.Getter;
 
-import static com.tsystems.tm.acc.tests.osr.network.line.profile.management.internal.v1_4_0.client.invoker.GsonObjectMapper.gson;
+import static com.tsystems.tm.acc.tests.osr.network.line.profile.management.v1_5_0.client.invoker.GsonObjectMapper.gson;
 import static io.restassured.RestAssured.config;
 import static io.restassured.config.ObjectMapperConfig.objectMapperConfig;
 
--- src/main/java/com/tsystems/tm/acc/ta/data/osr/models/NetworkLineProfileData.java
+++ src/main/java/com/tsystems/tm/acc/ta/data/osr/models/NetworkLineProfileData.java
@@ -1,8 +1,8 @@
 package com.tsystems.tm.acc.ta.data.osr.models;
 
-import com.tsystems.tm.acc.tests.osr.network.line.profile.management.internal.v1_4_0.client.model.ResourceCharacteristic;
-import com.tsystems.tm.acc.tests.osr.network.line.profile.management.internal.v1_4_0.client.model.ResourceOrder;
-import com.tsystems.tm.acc.tests.osr.network.line.profile.management.internal.v1_4_0.client.model.ResourceOrderItem;
+import com.tsystems.tm.acc.tests.osr.network.line.profile.management.v1_5_0.client.model.ResourceCharacteristic;
+import com.tsystems.tm.acc.tests.osr.network.line.profile.management.v1_5_0.client.model.ResourceOrder;
+import com.tsystems.tm.acc.tests.osr.network.line.profile.management.v1_5_0.client.model.ResourceOrderItem;
 import lombok.Data;
 
 import java.util.List;
