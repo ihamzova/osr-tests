72ef25c1a238826aedc3c5b9f68e85c4e52af88a
Irina Khamzova <irina.khamzova@t-systems.com>
2021-04-12 09:30:12 +0000

    DIGIHUB-83085: Test for cards updated

--- src/test/java/com/tsystems/tm/acc/ta/team/upiter/provisioning/OltProvisioning5600.java
+++ src/test/java/com/tsystems/tm/acc/ta/team/upiter/provisioning/OltProvisioning5600.java
@@ -32,112 +32,123 @@ import static com.tsystems.tm.acc.ta.data.upiter.UpiterConstants.*;
 })
 public class OltProvisioning5600 extends GigabitTest {
 
-    private static final Integer LATENCY_FOR_PORT_PROVISIONING = 100_000;
-    private static final Integer LATENCY_FOR_DEVICE_PROVISIONING = 15 * LATENCY_FOR_PORT_PROVISIONING;
+  private static final Integer LATENCY_FOR_PORT_PROVISIONING = 100_000;
+  private static final Integer LATENCY_FOR_DEVICE_PROVISIONING = 15 * LATENCY_FOR_PORT_PROVISIONING;
 
-    private AccessLineRiRobot accessLineRiRobot;
-    private WgAccessProvisioningRobot wgAccessProvisioningRobot;
-    private PortProvisioning portEmpty;
-    private PortProvisioning portProvisioningPartly;
-    private PortProvisioning portProvisioningFully;
-    private PortProvisioning portWithInActiveLines;
-    private UpiterTestContext context = UpiterTestContext.get();
+  private AccessLineRiRobot accessLineRiRobot;
+  private WgAccessProvisioningRobot wgAccessProvisioningRobot;
+  private PortProvisioning portEmpty;
+  private PortProvisioning portProvisioningPartly;
+  private PortProvisioning portProvisioningFully;
+  private PortProvisioning portWithInActiveLines;
+  private UpiterTestContext context = UpiterTestContext.get();
 
-    @BeforeMethod
-    public void prepareData() throws InterruptedException {
-        accessLineRiRobot.clearDatabase();
-        Thread.sleep(1000);
-        accessLineRiRobot.fillDatabaseForOltCommissioning();
-    }
+  @BeforeMethod
+  public void prepareData() throws InterruptedException {
+    accessLineRiRobot.clearDatabase();
+    Thread.sleep(1000);
+    accessLineRiRobot.fillDatabaseForOltCommissioning();
+  }
 
-    @AfterMethod
-    public void clearData() {
-        accessLineRiRobot.clearDatabase();
-    }
+  @AfterMethod
+  public void clearData() {
+    accessLineRiRobot.clearDatabase();
+  }
 
-    @BeforeClass
-    public void init() {
-        accessLineRiRobot = new AccessLineRiRobot();
-        wgAccessProvisioningRobot = new WgAccessProvisioningRobot();
-        portEmpty = context.getData().getPortProvisioningDataProvider().get(PortProvisioningCase.portEmpty5600);
-        portProvisioningPartly = context.getData().getPortProvisioningDataProvider().get(PortProvisioningCase.portPartlyOccupied);
-        portProvisioningFully = context.getData().getPortProvisioningDataProvider().get(PortProvisioningCase.portFullyOccupied);
-        portWithInActiveLines = context.getData().getPortProvisioningDataProvider().get(PortProvisioningCase.portWithInActiveLines);
-    }
+  @BeforeClass
+  public void init() {
+    accessLineRiRobot = new AccessLineRiRobot();
+    wgAccessProvisioningRobot = new WgAccessProvisioningRobot();
+    portEmpty = context.getData().getPortProvisioningDataProvider().get(PortProvisioningCase.portEmpty5600);
+    portProvisioningPartly = context.getData().getPortProvisioningDataProvider().get(PortProvisioningCase.portPartlyOccupied);
+    portProvisioningFully = context.getData().getPortProvisioningDataProvider().get(PortProvisioningCase.portFullyOccupied);
+    portWithInActiveLines = context.getData().getPortProvisioningDataProvider().get(PortProvisioningCase.portWithInActiveLines);
+  }
 
-    @Test
-    @TmsLink("DIGIHUB-29664")
-    @Description("Port provisioning case when port completely free")
-    public void portProvisioningEmpty() {
-        List<AccessLineDto> accessLinesBeforeProvisioning = accessLineRiRobot.getAccessLinesByPort(portEmpty);
-        Assert.assertEquals(accessLinesBeforeProvisioning.size(), 0);
+  @Test
+  @TmsLink("DIGIHUB-29664")
+  @Description("Port provisioning case when port completely free")
+  public void portProvisioningEmpty() {
+    List<AccessLineDto> accessLinesBeforeProvisioning = accessLineRiRobot.getAccessLinesByPort(portEmpty);
+    Assert.assertEquals(accessLinesBeforeProvisioning.size(), 0);
 
-        wgAccessProvisioningRobot.startPortProvisioning(portEmpty);
-        accessLineRiRobot.checkProvisioningResults(portEmpty);
-    }
+    wgAccessProvisioningRobot.startPortProvisioning(portEmpty);
+    accessLineRiRobot.checkProvisioningResults(portEmpty);
+  }
 
-    @Test
-    @TmsLink("DIGIHUB-32288")
-    @Description("Port provisioning case when port partly occupied")
-    public void portProvisioningPartly() {
-        List<AccessLineDto> accessLinesBeforeProvisioning = accessLineRiRobot.getAccessLinesByPort(portProvisioningPartly);
-        Assert.assertEquals(accessLinesBeforeProvisioning.size(), 8);
+  @Test
+  @TmsLink("DIGIHUB-32288")
+  @Description("Port provisioning case when port partly occupied")
+  public void portProvisioningPartly() {
+    List<AccessLineDto> accessLinesBeforeProvisioning = accessLineRiRobot.getAccessLinesByPort(portProvisioningPartly);
+    Assert.assertEquals(accessLinesBeforeProvisioning.size(), 8);
 
-        wgAccessProvisioningRobot.startPortProvisioning(portProvisioningPartly);
-        accessLineRiRobot.checkProvisioningResults(portProvisioningPartly);
-    }
+    wgAccessProvisioningRobot.startPortProvisioning(portProvisioningPartly);
+    accessLineRiRobot.checkProvisioningResults(portProvisioningPartly);
+  }
 
-    @Test
-    @TmsLink("DIGIHUB-40631")
-    @Description("Port provisioning case when port completely occupied")
-    public void portProvisioningFully() {
-        List<AccessLineDto> accessLinesBeforeProvisioning = accessLineRiRobot.getAccessLinesByPort(portProvisioningFully);
-        Assert.assertEquals(accessLinesBeforeProvisioning.size(), portProvisioningFully.getAccessLinesCount().intValue());
+  @Test
+  @TmsLink("DIGIHUB-40631")
+  @Description("Port provisioning case when port completely occupied")
+  public void portProvisioningFully() {
+    List<AccessLineDto> accessLinesBeforeProvisioning = accessLineRiRobot.getAccessLinesByPort(portProvisioningFully);
+    Assert.assertEquals(accessLinesBeforeProvisioning.size(), portProvisioningFully.getAccessLinesCount().intValue());
 
-        wgAccessProvisioningRobot.startPortProvisioning(portProvisioningFully);
-        accessLineRiRobot.checkProvisioningResults(portProvisioningFully);
-    }
+    wgAccessProvisioningRobot.startPortProvisioning(portProvisioningFully);
+    accessLineRiRobot.checkProvisioningResults(portProvisioningFully);
+  }
 
-    @Test
-    @TmsLink("DIGIHUB-32026")
-    @Description("Port provisioning case when port has InActive Lines")
-    public void portProvisioningWithInactiveLines() {
-        wgAccessProvisioningRobot.startPortProvisioning(portWithInActiveLines);
-        accessLineRiRobot.checkProvisioningResults(portWithInActiveLines);
-    }
+  @Test
+  @TmsLink("DIGIHUB-32026")
+  @Description("Port provisioning case when port has InActive Lines")
+  public void portProvisioningWithInactiveLines() {
+    wgAccessProvisioningRobot.startPortProvisioning(portWithInActiveLines);
+    accessLineRiRobot.checkProvisioningResults(portWithInActiveLines);
+  }
 
-    @Test
-    @TmsLink("DIGIHUB-29666")
-    @Description("Card provisioning case with 1 empty port")
-    public void cardProvisioning() {
-        Card cardBeforeProvisioning = wgAccessProvisioningRobot.getCard(portEmpty);
+  @Test
+  @TmsLink("DIGIHUB-29666")
+  @Description("Card provisioning case with 1 empty port")
+  public void cardProvisioning() {
+    Card cardBeforeProvisioning = wgAccessProvisioningRobot.getCard(portEmpty);
+    wgAccessProvisioningRobot.startCardProvisioning(portEmpty);
+    Assert.assertNotNull(cardBeforeProvisioning);
+    Assert.assertEquals(cardBeforeProvisioning.getPorts().size(), 1);
+    Assert.assertEquals(accessLineRiRobot.getAccessLinesByPort(portEmpty).size(), 0);
+    accessLineRiRobot.checkProvisioningResults(portEmpty);
+  }
 
-        Assert.assertNotNull(cardBeforeProvisioning);
-        Assert.assertEquals(cardBeforeProvisioning.getPorts().size(), 1);
-        Assert.assertEquals(accessLineRiRobot.getAccessLinesByPort(portEmpty).size(), 0);
+  @Test
+  @TmsLink("DIGIHUB-83085")
+  @Description("Card provisioning case with 1 card")
+  public void oneCardProvisioning() {
+    Card cardBeforeProvisioning = wgAccessProvisioningRobot.getCard(portEmpty);
+    wgAccessProvisioningRobot.startCardProvisioningV2(portEmpty);
+    Assert.assertNotNull(cardBeforeProvisioning);
+    Assert.assertEquals(cardBeforeProvisioning.getPorts().size(), 1);
+    Assert.assertEquals(accessLineRiRobot.getAccessLinesByPort(portEmpty).size(), 0);
+    accessLineRiRobot.checkProvisioningResults(portEmpty);
+  }
 
-        wgAccessProvisioningRobot.startCardProvisioning(portEmpty);
-        accessLineRiRobot.checkProvisioningResults(portEmpty);
-    }
 
-    @Test
-    @TmsLink("DIGIHUB-29667")
-    @Description("Device provisioning case")
-    public void deviceProvisioning() throws InterruptedException {
-        Device deviceBeforeProvisioning = wgAccessProvisioningRobot.getDevice(portEmpty);
+  @Test
+  @TmsLink("DIGIHUB-29667")
+  @Description("Device provisioning case")
+  public void deviceProvisioning() throws InterruptedException {
+    Device deviceBeforeProvisioning = wgAccessProvisioningRobot.getDevice(portEmpty);
 
-        Assert.assertNotNull(deviceBeforeProvisioning);
-        Assert.assertEquals(deviceBeforeProvisioning.getEmsNbiName(), "MA5600T");
-        Assert.assertEquals(deviceBeforeProvisioning.getEquipmentHolders().get(0).getCard().getPorts().size(), 8);
+    Assert.assertNotNull(deviceBeforeProvisioning);
+    Assert.assertEquals(deviceBeforeProvisioning.getEmsNbiName(), "MA5600T");
+    Assert.assertEquals(deviceBeforeProvisioning.getEquipmentHolders().get(0).getCard().getPorts().size(), 8);
 
-        wgAccessProvisioningRobot.startDeviceProvisioning(portEmpty);
-        Thread.sleep(LATENCY_FOR_DEVICE_PROVISIONING);
+    wgAccessProvisioningRobot.startDeviceProvisioning(portEmpty);
+    Thread.sleep(LATENCY_FOR_DEVICE_PROVISIONING);
 
-        Device deviceAfterProvisioning = wgAccessProvisioningRobot.getDevice(portEmpty);
+    Device deviceAfterProvisioning = wgAccessProvisioningRobot.getDevice(portEmpty);
 
-        PortProvisioning port = wgAccessProvisioningRobot.getPortProvisioning(portEmpty.getEndSz(),
-                deviceAfterProvisioning.getEquipmentHolders().get(2).getSlotNumber(),
-                deviceAfterProvisioning.getEquipmentHolders().get(2).getCard().getPorts().get(0).getPortNumber(), portEmpty);
-        accessLineRiRobot.checkProvisioningResults(port);
-    }
+    PortProvisioning port = wgAccessProvisioningRobot.getPortProvisioning(portEmpty.getEndSz(),
+            deviceAfterProvisioning.getEquipmentHolders().get(2).getSlotNumber(),
+            deviceAfterProvisioning.getEquipmentHolders().get(2).getCard().getPorts().get(0).getPortNumber(), portEmpty);
+    accessLineRiRobot.checkProvisioningResults(port);
+  }
 }
