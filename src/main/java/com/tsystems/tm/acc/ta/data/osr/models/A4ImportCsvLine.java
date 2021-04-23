e048c5ea97a755ef3be52c83470e5317bc6ea923
Swetlana Okonetschnikow <swetlana.okonetschnikow@t-systems.com>
2021-04-23 09:40:50 +0200

    DIGIHUB-99389 change structure of csv file

--- src/main/java/com/tsystems/tm/acc/ta/data/osr/models/A4ImportCsvLine.java
+++ src/main/java/com/tsystems/tm/acc/ta/data/osr/models/A4ImportCsvLine.java
@@ -6,17 +6,9 @@ import lombok.Data;
 public class A4ImportCsvLine {
 
     private String negName;
-//    private String negCno;
-//    private String negDescription;
+    private String negDescription;
     private String neVpsz;
     private String neFsz;
-//    private String nePlanningDeviceName;
-    private String neType;
-//    private String neVsp;
-    private String neKlsId;
-//    private String neLocAddress;
-//    private String neLocRackId;
-//    private String neLocRackPosition;
     private String neDescription;
 
 }
