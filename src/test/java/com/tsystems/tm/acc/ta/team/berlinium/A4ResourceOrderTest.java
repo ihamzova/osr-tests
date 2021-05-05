8efe8a1839578b358c46bb54246f0cda87be2c50
heiko.schwanke <heiko.schwanke@t-systems.com>
2021-05-05 15:34:41 +0200

    DIGIHUB-105760 create osr-tests for a10nsp resource order

---
+++ src/test/java/com/tsystems/tm/acc/ta/team/berlinium/A4ResourceOrderTest.java
@@ -0,0 +1,63 @@
+package com.tsystems.tm.acc.ta.team.berlinium;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import org.testng.annotations.Test;
+
+public class A4ResourceOrderTest {
+
+// before, test data
+
+// after, clean
+
+
+
+
+    @Test
+    @Owner("heiko.schwanke@t-systems.com")
+    @Description("a10-switch in resource order from Merlin is unknown")
+    public void testUnknownSwitch() {
+        //
+
+    }
+
+    @Test
+    @Owner("heiko.schwanke@t-systems.com")
+    @Description("rebell-link for resource order from Merlin is unknown")
+    public void testUnknownNel() {
+        //
+
+    }
+
+    @Test
+    @Owner("heiko.schwanke@t-systems.com")
+    @Description("add-case: NSP of a10nsp changed to INSTALLING")
+    public void testAddLink() {
+        //
+
+    }
+
+    @Test
+    @Owner("heiko.schwanke@t-systems.com")
+    @Description("modify-case: NSP of a10nsp remains OPERATING")
+    public void testModifyLink() {
+        //
+
+    }
+
+    @Test
+    @Owner("heiko.schwanke@t-systems.com")
+    @Description("delete-case: NSP of a10nsp changed to DEACTIVATED")
+    public void testDeleteLink() {
+        //
+
+    }
+
+// common methods
+
+    // POST CBR/RV to Mercury
+
+    // Update state to a4-roi
+
+    // cb to merlin
+}
