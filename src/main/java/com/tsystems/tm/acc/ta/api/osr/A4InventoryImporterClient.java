a8a204ba9dafe5eacc7b05c7e0933eb5901bccdd
Anita Junge <anita.junge@t-systems.com>
2021-11-02 11:35:27 +0100

    DIGIHUB-125945 implement ApiClient and Robot for A4DpuCommissioning

---
+++ src/main/java/com/tsystems/tm/acc/ta/api/osr/A4InventoryImporterClient.java
@@ -0,0 +1,53 @@
+package com.tsystems.tm.acc.ta.api.osr;
+
+import com.tsystems.tm.acc.ta.api.AuthTokenProvider;
+import com.tsystems.tm.acc.ta.api.BearerHeaderAuthTokenInjector;
+import com.tsystems.tm.acc.ta.api.RequestSpecBuilders;
+import com.tsystems.tm.acc.ta.api.Resetable;
+import com.tsystems.tm.acc.ta.url.GigabitUrlBuilder;
+import com.tsystems.tm.acc.tests.osr.a4.inventory.importer.client.invoker.ApiClient;
+import com.tsystems.tm.acc.tests.osr.a4.inventory.importer.client.invoker.JSON;
+import com.tsystems.tm.acc.tests.osr.a4.resource.inventory.client.invoker.GsonObjectMapper;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import lombok.Getter;
+
+import static com.tsystems.tm.acc.ta.data.osr.DomainConstants.A4_INVENTORY_IMPORTER_MS;
+import static com.tsystems.tm.acc.tests.osr.a4.inventory.importer.client.invoker.GsonObjectMapper.gson;
+import static io.restassured.RestAssured.config;
+import static io.restassured.config.ObjectMapperConfig.objectMapperConfig;
+
+@Getter
+public class A4InventoryImporterClient implements Resetable {
+
+    private final ApiClient client;
+
+    public A4InventoryImporterClient() {
+        client = ApiClient.api(ApiClient.Config.apiConfig().reqSpecSupplier(
+                () -> new RequestSpecBuilder().setConfig(config().objectMapperConfig(objectMapperConfig().defaultObjectMapper(gson())))
+                        .addFilter(new RequestLoggingFilter())
+                        .addFilter(new ResponseLoggingFilter())
+                        .addHeader("Content-Type", "application/json")
+                        .setBaseUri(new GigabitUrlBuilder(A4_INVENTORY_IMPORTER_MS).buildUri())));
+    }
+
+    public A4InventoryImporterClient(AuthTokenProvider authTokenProvider){
+        client = ApiClient.api(ApiClient.Config.apiConfig().reqSpecSupplier(
+                () -> RequestSpecBuilders.getDefaultWithAuth(
+                        GsonObjectMapper.gson(),
+                        new GigabitUrlBuilder(A4_INVENTORY_IMPORTER_MS).buildUri(),
+                        new BearerHeaderAuthTokenInjector(authTokenProvider)
+                )
+        ));
+    }
+
+    public static JSON json() {
+        return new JSON();
+    }
+
+    @Override
+    public void reset() {
+    }
+
+}
