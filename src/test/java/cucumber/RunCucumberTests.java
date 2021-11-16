10c3e94046800de1481a972e01e5d14a552e9929
Bela Kovac <bela.kovac@t-systems.com>
2021-11-16 07:54:26 +0100

    DIGIHUB-127317

--- src/test/java/cucumber/RunCucumberTests.java
+++ src/test/java/cucumber/RunCucumberTests.java
@@ -13,16 +13,14 @@ import org.testng.annotations.DataProvider;
                 "html:target/allure-results/cucumberscenarios.html",
                 "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm",
                 "json:target/allure-results/cucumberscenarios.json"},
-//        plugin = { "pretty", "json:target/allure-results/cucumberscenarios.json" },
         monochrome = true,
         glue = { "cucumber/stepdefinitions" },
         features = { "src/test/resources/cucumberscenarios" }
-//        tags = ""
 )
 public class RunCucumberTests extends AbstractTestNGCucumberTests {
 
     @Override
-    @DataProvider() // (parallel = true) <- use this if you want to execute multiple scenarios in parallel
+    @DataProvider() // @DataProvider(parallel = true) <- use this if you want to execute multiple scenarios in parallel
     public Object[][] scenarios() {
         return super.scenarios();
     }
