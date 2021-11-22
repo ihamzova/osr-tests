4fb3d1fbb18456b594627e8ff5a02da409a98064
Bela Kovac <bela.kovac@t-systems.com>
2021-11-22 08:37:23 +0100

    DIGIHUB-127783 shortened variable names

--- src/test/java/cucumber/stepdefinitions/team/berlinium/A4RevInvSteps.java
+++ src/test/java/cucumber/stepdefinitions/team/berlinium/A4RevInvSteps.java
@@ -48,17 +48,17 @@ public class A4RevInvSteps extends BaseSteps {
 
     @Given("no TP exists in A4 resource inventory")
     public void noTPExistsInA4ResourceInventory() {
-        A4TerminationPoint tpData = new A4TerminationPoint();
-        tpData.setUuid(UUID.randomUUID().toString());
-        getScenarioContext().setContext(Context.A4_TP, tpData);
+        A4TerminationPoint tp = new A4TerminationPoint();
+        tp.setUuid(UUID.randomUUID().toString());
+        getScenarioContext().setContext(Context.A4_TP, tp);
     }
 
     @Given("a TP is existing in A4 resource inventory")
     public void aTPIsExistingInA4ResourceInventory() {
-        A4TerminationPoint tpData = setupDefaultTpTestData();
+        A4TerminationPoint tp = setupDefaultTpTestData();
         A4NetworkElementPort nep = (A4NetworkElementPort) getScenarioContext().getContext(Context.A4_NEP);
-        getScenarioContext().setContext(Context.A4_TP, tpData);
-        a4ResInv.createTerminationPoint(tpData, nep);
+        getScenarioContext().setContext(Context.A4_TP, tp);
+        a4ResInv.createTerminationPoint(tp, nep);
     }
 
     @Given("a NEP is existing in A4 resource inventory")
@@ -67,11 +67,11 @@ public class A4RevInvSteps extends BaseSteps {
         if(!getScenarioContext().isContains(Context.A4_NE))
             aNEIsExistingInA4ResourceInventory();
 
-        A4NetworkElementPort nepData = osrTestContext.getData().getA4NetworkElementPortDataProvider()
+        A4NetworkElementPort nep = osrTestContext.getData().getA4NetworkElementPortDataProvider()
                 .get(A4NetworkElementPortCase.defaultNetworkElementPort);
-        getScenarioContext().setContext(Context.A4_NEP, nepData);
+        getScenarioContext().setContext(Context.A4_NEP, nep);
         A4NetworkElement ne = (A4NetworkElement) getScenarioContext().getContext(Context.A4_NE);
-        a4ResInv.createNetworkElementPort(nepData, ne);
+        a4ResInv.createNetworkElementPort(nep, ne);
     }
 
     @Given("a NE is existing in A4 resource inventory")
@@ -80,19 +80,19 @@ public class A4RevInvSteps extends BaseSteps {
         if (!getScenarioContext().isContains(Context.A4_NEG))
             aNEGIsExistingInA4ResourceInventory();
 
-        A4NetworkElement neData = osrTestContext.getData().getA4NetworkElementDataProvider()
+        A4NetworkElement ne = osrTestContext.getData().getA4NetworkElementDataProvider()
                 .get(A4NetworkElementCase.defaultNetworkElement);
-        getScenarioContext().setContext(Context.A4_NE, neData);
+        getScenarioContext().setContext(Context.A4_NE, ne);
         A4NetworkElementGroup neg = (A4NetworkElementGroup) getScenarioContext().getContext(Context.A4_NEG);
-        a4ResInv.createNetworkElement(neData, neg);
+        a4ResInv.createNetworkElement(ne, neg);
     }
 
     @Given("a NEG is existing in A4 resource inventory")
     public void aNEGIsExistingInA4ResourceInventory() {
-        A4NetworkElementGroup negData = osrTestContext.getData().getA4NetworkElementGroupDataProvider()
+        A4NetworkElementGroup neg = osrTestContext.getData().getA4NetworkElementGroupDataProvider()
                 .get(A4NetworkElementGroupCase.defaultNetworkElementGroup);
-        getScenarioContext().setContext(Context.A4_NEG, negData);
-        a4ResInv.createNetworkElementGroup(negData);
+        getScenarioContext().setContext(Context.A4_NEG, neg);
+        a4ResInv.createNetworkElementGroup(neg);
     }
 
     @Given("a NSP FTTH(-Access) with Line ID {string} is existing in A4 resource inventory( for the TP)")
@@ -101,29 +101,29 @@ public class A4RevInvSteps extends BaseSteps {
         if (!getScenarioContext().isContains(Context.A4_TP))
             aTPIsExistingInA4ResourceInventory();
 
-        A4NetworkServiceProfileFtthAccess nspFtthData = osrTestContext.getData()
+        A4NetworkServiceProfileFtthAccess nspFtth = osrTestContext.getData()
                 .getA4NetworkServiceProfileFtthAccessDataProvider()
                 .get(A4NetworkServiceProfileFtthAccessCase.defaultNetworkServiceProfileFtthAccess);
-        nspFtthData.setLineId(lineId);
-        getScenarioContext().setContext(Context.A4_NSP_FTTH, nspFtthData);
+        nspFtth.setLineId(lineId);
+        getScenarioContext().setContext(Context.A4_NSP_FTTH, nspFtth);
         A4TerminationPoint tp = (A4TerminationPoint) getScenarioContext().getContext(Context.A4_TP);
-        a4ResInv.createNetworkServiceProfileFtthAccess(nspFtthData, tp);
+        a4ResInv.createNetworkServiceProfileFtthAccess(nspFtth, tp);
     }
 
     @Given("no NSP FTTH(-Access) exists in A4 resource inventory( for the TP)")
     public void noNSPFTTHExistsInA4ResourceInventoryForTheTP() {
-        A4NetworkServiceProfileFtthAccess nspFtthData = new A4NetworkServiceProfileFtthAccess();
-        nspFtthData.setUuid(UUID.randomUUID().toString());
-        getScenarioContext().setContext(Context.A4_NSP_FTTH, nspFtthData);
+        A4NetworkServiceProfileFtthAccess nspFtth = new A4NetworkServiceProfileFtthAccess();
+        nspFtth.setUuid(UUID.randomUUID().toString());
+        getScenarioContext().setContext(Context.A4_NSP_FTTH, nspFtth);
     }
 
     @Given("a TP with type {string} is existing in A4 resource inventory")
     public void aTPWithTypeIsExistingInA4ResourceInventory(String tpType) {
-        A4TerminationPoint tpData = setupDefaultTpTestData();
-        tpData.setSubType(tpType);
-        getScenarioContext().setContext(Context.A4_TP, tpData);
+        A4TerminationPoint tp = setupDefaultTpTestData();
+        tp.setSubType(tpType);
+        getScenarioContext().setContext(Context.A4_TP, tp);
         A4NetworkElementPort nep = (A4NetworkElementPort) getScenarioContext().getContext(Context.A4_NEP);
-        a4ResInv.createTerminationPoint(tpData, nep);
+        a4ResInv.createTerminationPoint(tp, nep);
     }
 
     @Then("the TP does not exist in A4 resource inventory( anymore)/( any longer)")
