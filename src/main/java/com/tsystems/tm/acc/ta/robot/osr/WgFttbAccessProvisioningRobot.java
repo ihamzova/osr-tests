f9973f6e8d07e27533ea8d793cec3fbc7ce4189a
Alina Maiorova <alina.maiorova@t-systems.com>
2021-11-30 15:17:40 +0000

    DIGIHUB-122629: dpu (de)provisioning on adtran

--- src/main/java/com/tsystems/tm/acc/ta/robot/osr/WgFttbAccessProvisioningRobot.java
+++ src/main/java/com/tsystems/tm/acc/ta/robot/osr/WgFttbAccessProvisioningRobot.java
@@ -2,6 +2,7 @@ package com.tsystems.tm.acc.ta.robot.osr;
 
 import com.tsystems.tm.acc.ta.api.AuthTokenProvider;
 import com.tsystems.tm.acc.ta.api.RhssoClientFlowAuthTokenProvider;
+import com.tsystems.tm.acc.ta.api.UnleashClient;
 import com.tsystems.tm.acc.ta.api.osr.WgFttbAccessProvisioningClient;
 import com.tsystems.tm.acc.ta.data.osr.models.PortProvisioning;
 import com.tsystems.tm.acc.ta.helpers.RhssoHelper;
@@ -13,6 +14,7 @@ import java.util.UUID;
 
 import static com.tsystems.tm.acc.ta.api.ResponseSpecBuilders.shouldBeCode;
 import static com.tsystems.tm.acc.ta.api.ResponseSpecBuilders.validatedWith;
+import static com.tsystems.tm.acc.ta.data.osr.DomainConstants.FEATURE_TOGGLE_DPU_LIFECYCLE_USES_DPU_DEMANDS_NAME;
 import static com.tsystems.tm.acc.ta.data.upiter.CommonTestData.HTTP_CODE_ACCEPTED_202;
 import static com.tsystems.tm.acc.ta.wiremock.ExtendedWireMock.CONSUMER_ENDPOINT;
 
@@ -22,6 +24,7 @@ public class WgFttbAccessProvisioningRobot {
     private static final UUID uuid = UUID.randomUUID();
     private static final AuthTokenProvider authTokenProvider = new RhssoClientFlowAuthTokenProvider("wiremock-acc", RhssoHelper.getSecretOfGigabitHub("wiremock-acc"));
     private WgFttbAccessProvisioningClient wgFttbAccessProvisioningClient = new WgFttbAccessProvisioningClient(authTokenProvider);
+    private UnleashClient unleashClient = new UnleashClient();
 
     @Step("Start FTTB preprovisioning process for a device")
     public void startWgFttbAccessProvisioningForDevice(String dpuEndSz) {
@@ -82,6 +85,21 @@ public class WgFttbAccessProvisioningRobot {
                 .dpuEndSZQuery(dpuEndSz)
                 .execute(validatedWith(shouldBeCode(HTTP_CODE_ACCEPTED_202)));
         log.info("Received xCallbackCorrelationId: " + uuid.toString());
+    }
+
+    @Step("Change feature toggle state")
+    public void changeFeatureToogleDpuDemandState(boolean toggleState) {
+        if (toggleState) {
+            unleashClient.enableToggle(FEATURE_TOGGLE_DPU_LIFECYCLE_USES_DPU_DEMANDS_NAME);
+        } else {
+            unleashClient.disableToggle(FEATURE_TOGGLE_DPU_LIFECYCLE_USES_DPU_DEMANDS_NAME);
+        }
+        log.info("toggleState for {} = {}", FEATURE_TOGGLE_DPU_LIFECYCLE_USES_DPU_DEMANDS_NAME , toggleState);
+    }
+
+    @Step("Get feature toggle state")
+    public boolean getFeatureToggleDpuDemandState() {
+        return unleashClient.isToggleEnabled(FEATURE_TOGGLE_DPU_LIFECYCLE_USES_DPU_DEMANDS_NAME);
     }
 
 
