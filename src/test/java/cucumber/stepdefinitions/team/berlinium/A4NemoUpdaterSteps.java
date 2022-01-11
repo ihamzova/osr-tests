5f5c90958cd944ade4e6e16bdd2d65b127a8d4d9
Bela Kovac <bela.kovac@t-systems.com>
2022-01-11 15:08:42 +0100

    DIGIHUB-128553 Tuning

--- src/test/java/cucumber/stepdefinitions/team/berlinium/A4NemoUpdaterSteps.java
+++ src/test/java/cucumber/stepdefinitions/team/berlinium/A4NemoUpdaterSteps.java
@@ -1,30 +1,18 @@
 package cucumber.stepdefinitions.team.berlinium;
 
-import com.tsystems.tm.acc.data.osr.models.a4networkelementgroup.A4NetworkElementGroupCase;
-import com.tsystems.tm.acc.data.osr.models.a4terminationpoint.A4TerminationPointCase;
 import com.tsystems.tm.acc.ta.data.osr.models.A4NetworkElementGroup;
-import com.tsystems.tm.acc.ta.data.osr.models.A4NetworkElementPort;
-import com.tsystems.tm.acc.ta.data.osr.models.A4NetworkServiceProfileFtthAccess;
-import com.tsystems.tm.acc.ta.data.osr.models.A4TerminationPoint;
 import com.tsystems.tm.acc.ta.robot.osr.A4NemoUpdaterRobot;
-import com.tsystems.tm.acc.ta.robot.osr.A4ResourceInventoryServiceRobot;
 import cucumber.BaseSteps;
 import cucumber.Context;
 import cucumber.TestContext;
 import de.telekom.it.t3a.kotlin.log.annotations.ServiceLog;
-import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.restassured.response.Response;
 
 import static com.tsystems.tm.acc.ta.data.osr.DomainConstants.A4_NEMO_UPDATER_MS;
-import static com.tsystems.tm.acc.ta.data.osr.DomainConstants.A4_RESOURCE_INVENTORY_SERVICE_MS;
-import static com.tsystems.tm.acc.ta.robot.utils.MiscUtils.sleepForSeconds;
 
 @ServiceLog({A4_NEMO_UPDATER_MS})
 public class A4NemoUpdaterSteps extends BaseSteps {
 
-    final int SLEEP_TIMER = 5;
-    private final A4ResourceInventoryServiceRobot a4ResInvService = new A4ResourceInventoryServiceRobot();
     private final A4NemoUpdaterRobot a4NemoUpdater = new A4NemoUpdaterRobot();
 
     public A4NemoUpdaterSteps(TestContext testContext) {
@@ -33,17 +21,13 @@ public class A4NemoUpdaterSteps extends BaseSteps {
 
     // -----=====[ WHENS ]=====-----
 
-    @When("Trigger an Update Call to Nemo for NetworkElementGroup")
+    @When("an update call to Nemo for the NEG is triggered")
     public void whenTriggerAnUpdateCallToNemoForNEG() {
         // INPUT FROM SCENARIO CONTEXT
         final A4NetworkElementGroup neg = (A4NetworkElementGroup) getScenarioContext().getContext(Context.A4_NEG);
 
         // ACTION
         a4NemoUpdater.triggerNemoUpdate(neg.getUuid());
-
-        // OUTPUT INTO SCENARIO CONTEXT
-        //getScenarioContext().setContext(Context.A4_NEG, neg);
-
     }
 
 }
