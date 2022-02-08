bd5c7de0a5bb47004f228ea680a4511543135055
Bela Kovac <bela.kovac@t-systems.com>
2022-02-08 19:19:25 +0100

    DIGIHUB-130145 Use AMQ-HA instead of AMQ now

--- src/main/java/com/tsystems/tm/acc/ta/robot/osr/A4ResilienceRobot.java
+++ src/main/java/com/tsystems/tm/acc/ta/robot/osr/A4ResilienceRobot.java
@@ -193,17 +193,17 @@ public class A4ResilienceRobot {
 
     private String getQueueUrl(String queue) {
         // Activate this when using AMQ
-        return new GigabitUrlBuilder(A4_QUEUE_DISPATCHER_QUEUE).withoutSuffix().buildUri()
-                + "/console/jolokia/exec/org.apache.activemq.artemis:broker=%22broker%22,component=addresses,address=%22"
-                + queue + "%22,subcomponent=queues,routing-type=%22anycast%22,queue=%22"
-                + queue + "%22/";
+//        return new GigabitUrlBuilder(A4_QUEUE_DISPATCHER_QUEUE).withoutSuffix().buildUri()
+//                + "/console/jolokia/exec/org.apache.activemq.artemis:broker=%22broker%22,component=addresses,address=%22"
+//                + queue + "%22,subcomponent=queues,routing-type=%22anycast%22,queue=%22"
+//                + queue + "%22/";
 
         // Activate this when using AMQ-HA
-//        return new GigabitUrlBuilder(A4_QUEUE_DISPATCHER_QUEUE).withoutSuffix().buildUri()
-//                + "/console/jolokia/exec/org.apache.activemq.artemis:broker=!%22"
-//                + A4_QUEUE_DISPATCHER_QUEUE + "!%22,component=addresses,address=!%22"
-//                + queue + "!%22,subcomponent=queues,routing-type=!%22anycast!%22,queue=!%22"
-//                + queue + "!%22/";
+        return new GigabitUrlBuilder(A4_QUEUE_DISPATCHER_QUEUE).withoutSuffix().buildUri()
+                + "/console/jolokia/exec/org.apache.activemq.artemis:broker=!%22"
+                + A4_QUEUE_DISPATCHER_QUEUE + "!%22,component=addresses,address=!%22"
+                + queue + "!%22,subcomponent=queues,routing-type=!%22anycast!%22,queue=!%22"
+                + queue + "!%22/";
     }
 
 }
