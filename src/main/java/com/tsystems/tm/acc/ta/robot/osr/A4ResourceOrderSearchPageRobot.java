3950701b36081eb38f64ca0d203b79b2c63ca313
bela.kovac <bela.kovac@t-systems.com>
2022-04-26 12:51:56 +0200

    DIGIHUB-142572 Code cleanup

--- src/main/java/com/tsystems/tm/acc/ta/robot/osr/A4ResourceOrderSearchPageRobot.java
+++ src/main/java/com/tsystems/tm/acc/ta/robot/osr/A4ResourceOrderSearchPageRobot.java
@@ -1,63 +1,59 @@
 package com.tsystems.tm.acc.ta.robot.osr;
 
-
 import com.codeborne.selenide.ElementsCollection;
-import com.tsystems.tm.acc.ta.pages.osr.a4resourceinventory.A4MobileMonitoringPage;
 import com.tsystems.tm.acc.ta.pages.osr.a4resourceinventory.A4ResourceOrderSearchPage;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
+import static org.testng.AssertJUnit.fail;
 
 public class A4ResourceOrderSearchPageRobot {
 
     @Step("Open UI, log in, and goTo RO-Search-page")
-    public void openRoSearchPage(){
+    public void openRoSearchPage() {
         A4ResourceOrderSearchPage
                 .login();
     }
 
     @Step("Enter vuep")
-    public void enterRoVuep(String value) { $(A4ResourceOrderSearchPage.getRO_VUEP_NUMBER_FIELD_LOCATOR()).val(value); }
+    public void enterRoVuep(String value) {
+        $(A4ResourceOrderSearchPage.getRO_VUEP_NUMBER_FIELD_LOCATOR()).val(value);
+    }
 
     @Step("Select InProgress")
-    public void selectInProgress() { $(A4ResourceOrderSearchPage.getRO_CHECKBOX_IN_PROGRESS()).click(); }
+    public void selectInProgress() {
+        $(A4ResourceOrderSearchPage.getRO_CHECKBOX_IN_PROGRESS()).click();
+    }
 
     @Step("Select Completed")
-    public void selectCompleted() { $(A4ResourceOrderSearchPage.getRO_CHECKBOX_COMPLETED()).click(); }
+    public void selectCompleted() {
+        $(A4ResourceOrderSearchPage.getRO_CHECKBOX_COMPLETED()).click();
+    }
 
     @Step("Select Rejected")
-    public void selectRejected() { $(A4ResourceOrderSearchPage.getRO_CHECKBOX_REJECTED()).click(); }
-
+    public void selectRejected() {
+        $(A4ResourceOrderSearchPage.getRO_CHECKBOX_REJECTED()).click();
+    }
 
     @Step("Click search button")
     public void clickRoSearchButton() {
         $(A4ResourceOrderSearchPage.getRO_SEARCH_BUTTON_LOCATOR()).click();
     }
 
-    @Step("Click Row 1")
-    public void clickSearchResultRow1() {
-        $(A4ResourceOrderSearchPage.getRO_TABLE_ROW_1()).click();
-        //$(A4ResourceOrderSearchPage.getRO_TABLE_ROW_1()).doubleClick();
-    }
-
-
     public void clickDetailLinkForFirstROInSearchResultTable() {
-        // getRoElementsCollection().get(0).click();
-
         $(A4ResourceOrderSearchPage.getRO_SEARCH_RESULT_TABLE_LOCATOR())
                 .toWebElement().findElement(A4ResourceOrderSearchPage.RO_DETAIL_LINK_LOCATOR_1).click();
     }
 
-
     public ElementsCollection getRoElementsCollection() {
-        // waitForTableToFullyLoad(elementsCollection.size());
         try {
             Thread.sleep(2000);
             return $(A4ResourceOrderSearchPage.getRO_SEARCH_RESULT_TABLE_LOCATOR())
                     .findAll(By.xpath("tr/td"));
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            fail("Unexpected exceoption: " + e.getMessage());
+            Thread.currentThread().interrupt();
             return null;
         }
     }
