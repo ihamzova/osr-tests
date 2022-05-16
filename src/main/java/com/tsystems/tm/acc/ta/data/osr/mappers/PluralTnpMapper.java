8558704434aea04f33e015315fb2c729e5aebf23
Swetlana Okonetschnikow <swetlana.okonetschnikow@t-systems.com>
2022-05-16 17:16:53 +0200

    DIGIHUB-151066 change PluralTnpMapper for a new plural json

--- src/main/java/com/tsystems/tm/acc/ta/data/osr/mappers/PluralTnpMapper.java
+++ src/main/java/com/tsystems/tm/acc/ta/data/osr/mappers/PluralTnpMapper.java
@@ -1,30 +1,30 @@
 package com.tsystems.tm.acc.ta.data.osr.mappers;
 
 import com.tsystems.tm.acc.ta.data.osr.models.A4ImportCsvData;
-import com.tsystems.tm.acc.tests.osr.plural.tnp.client.model.JsonIRsrv1KnotenLesenD3Response;
-import com.tsystems.tm.acc.tests.osr.plural.tnp.client.model.JsonIRsrv1KnotenLesenD3ResponseErep1Knoten;
-import com.tsystems.tm.acc.tests.osr.plural.tnp.client.model.JsonIRsrv1KnotenLesenD3ResponseExp1Knoten;
-import com.tsystems.tm.acc.tests.osr.plural.tnp.client.model.JsonIRsrv1KnotenLesenD3ResponseExp1Vpsz;
+import com.tsystems.tm.acc.tests.osr.plural.tnp.client.model.Rsrv1KnotenLesenD3Erep1KnotenItem;
+import com.tsystems.tm.acc.tests.osr.plural.tnp.client.model.Rsrv1KnotenLesenD3Erep1KnotenItemExp1Knoten;
+import com.tsystems.tm.acc.tests.osr.plural.tnp.client.model.Rsrv1KnotenLesenD3Erep1KnotenItemExp1Vpsz;
+import com.tsystems.tm.acc.tests.osr.plural.tnp.client.model.Rsrv1KnotenLesenD3Response;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class PluralTnpMapper {
 
-    public JsonIRsrv1KnotenLesenD3Response getJsonIRsrv1KnotenLesenD3Response(A4ImportCsvData csvData){
-        List<JsonIRsrv1KnotenLesenD3ResponseErep1Knoten> erep1KnotenList = new ArrayList<>();
+    public Rsrv1KnotenLesenD3Response getJsonIRsrv1KnotenLesenD3Response(A4ImportCsvData csvData){
+        List<Rsrv1KnotenLesenD3Erep1KnotenItem> erep1KnotenList = new ArrayList<>();
         csvData.getCsvLines().forEach(csvLine -> {
-            JsonIRsrv1KnotenLesenD3ResponseExp1Vpsz exp1Vpsz = new JsonIRsrv1KnotenLesenD3ResponseExp1Vpsz()
+            Rsrv1KnotenLesenD3Erep1KnotenItemExp1Vpsz exp1Vpsz = new Rsrv1KnotenLesenD3Erep1KnotenItemExp1Vpsz()
                     .vpsz(csvLine.getNeVpsz());
-            JsonIRsrv1KnotenLesenD3ResponseExp1Knoten exp1Knoten = new JsonIRsrv1KnotenLesenD3ResponseExp1Knoten()
+            Rsrv1KnotenLesenD3Erep1KnotenItemExp1Knoten exp1Knoten = new Rsrv1KnotenLesenD3Erep1KnotenItemExp1Knoten()
                     .fachsz(csvLine.getNeFsz());
-            JsonIRsrv1KnotenLesenD3ResponseErep1Knoten erep1Knoten = new JsonIRsrv1KnotenLesenD3ResponseErep1Knoten()
+            Rsrv1KnotenLesenD3Erep1KnotenItem erep1Knoten = new Rsrv1KnotenLesenD3Erep1KnotenItem()
                     .exp1Vpsz(exp1Vpsz)
                     .exp1Knoten(exp1Knoten);
             erep1KnotenList.add(erep1Knoten);
         });
 
-        return new JsonIRsrv1KnotenLesenD3Response()
+        return new Rsrv1KnotenLesenD3Response()
                 .command("ANZEIGEN")
                 .erep1Knoten(erep1KnotenList);
     }
