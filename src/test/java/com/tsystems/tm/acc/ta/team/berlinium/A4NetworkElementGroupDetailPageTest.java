dd57e75acd1e46ecc101f926aa9e571817be2f58
Jürgen Mayer <juergen.mayer@t-systems.com>
2022-05-23 13:31:26 +0200

    DIGIHUB-62776 sleep 6 sec

--- src/test/java/com/tsystems/tm/acc/ta/team/berlinium/A4NetworkElementGroupDetailPageTest.java
+++ src/test/java/com/tsystems/tm/acc/ta/team/berlinium/A4NetworkElementGroupDetailPageTest.java
@@ -104,7 +104,7 @@ public class A4NetworkElementGroupDetailPageTest extends GigabitTest {
         // WHEN
 
         a4ResourceInventoryNegDetails.clickNeIcon();
-        sleepForSeconds(4);  // wait for result
+        sleepForSeconds(6);  // wait for result
         // THEN
         a4ResourceInventoryNeDetails.checkLandedOnCorrectNeDetailsPage(neDataA);
 
