cafe1f2311f40b31aabb6fe454417c902ff59209
Alina Maiorova <alina.maiorova@t-systems.com>
2022-06-10 16:31:03 +0000

    DIGIHUB-145069: refactoring of ont usage and ont commissioning autotests

--- src/test/java/com/tsystems/tm/acc/ta/team/upiter/ontcommissioning/OntCommissioning.java
+++ src/test/java/com/tsystems/tm/acc/ta/team/upiter/ontcommissioning/OntCommissioning.java
@@ -265,7 +265,7 @@ public class OntCommissioning extends GigabitTest {
   public void ontDecommissioning33LineToggleOffTest() throws InterruptedException {
     wgAccessProvisioningRobot.changeFeatureToogleEnable64PonSplittingState(false);
 
-   // Thread.sleep(1000);
+    Thread.sleep(5000);
 
     // prepare test data
     List<AccessLineDto> accessLines = accessLineRiRobot.getAccessLinesByPort(port).stream()
@@ -292,7 +292,6 @@ public class OntCommissioning extends GigabitTest {
     assertEquals("HomeId on the AccessLine that wasn't decommissioned is incorrect",
             accessLineRiRobot.getAccessLinesByLineId(accessLineFor33LineCaseOld.getLineId()).get(0).getHomeId(),
             accessLineFor33LineCaseOld.getHomeId());
-    Thread.sleep(5000);
 
     assertTrue(accessLineRiRobot.getAccessLinesByLineId(accessLineFor33LineCaseNew.getLineId()).isEmpty(), "Decommissioned AccessLine wasn't deleted");
   }
