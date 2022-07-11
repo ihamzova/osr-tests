c1057435f3571de11400d9dc5963c7a93cac2cf0
Anna Terenteva <anna.terenteva@telekom.com>
2022-07-11 19:31:35 +0300

    DIGIHUB-0000 [force publish] fix OltCommissioningClient

--- src/main/java/com/tsystems/tm/acc/ta/robot/osr/OltCommissioningRobot.java
+++ src/main/java/com/tsystems/tm/acc/ta/robot/osr/OltCommissioningRobot.java
@@ -95,10 +95,10 @@ public class OltCommissioningRobot {
                         .orderNumber(Integer.parseInt(oltDevice.getOrderNumber())));
 
         return new OltCommissioningClient(tokenProvider, tokenProvider).getClient()
-                .oltCommissioningV2()
-                .startDeviceCommissioningV2()
-                .body(oltCommissioningDto)
-                .executeAs(checkStatus(HTTP_CODE_ACCEPTED_202)).getId().toString();
+                                                                       .oltCommissioningV2()
+                                                                       .startDeviceCommissioningV2()
+                                                                       .body(oltCommissioningDto)
+                                                                       .executeAs(checkStatus(HTTP_CODE_ACCEPTED_202)).getId().toString();
     }
 
     @Owner("TMI")
@@ -111,13 +111,13 @@ public class OltCommissioningRobot {
                 .and().pollInSameThread()
                 .untilAsserted(() -> {
                     ProcessLogListItemDTO processLog = osrProcessLogClient.getClient().centralProcessLog()
-                            .getProcessLogs()
-                            .processInstanceIdQuery(processId)
-                            .domainReferenceIdQuery(oltEndSz)
-                            .executeAs(checkStatus(HTTP_CODE_OK_200))
-                            .getProcesslogs().stream()
-                            .filter(process -> Objects.equals(process.getProcessId(), "OltCommissioning"))
-                            .findAny().orElseThrow(() -> new AssertionError("Olt commissioning processId wasn't found"));
+                                                                          .getProcessLogs()
+                                                                          .processInstanceIdQuery(processId)
+                                                                          .domainReferenceIdQuery(oltEndSz)
+                                                                          .executeAs(checkStatus(HTTP_CODE_OK_200))
+                                                                          .getProcesslogs().stream()
+                                                                          .filter(process -> Objects.equals(process.getProcessId(), "OltCommissioning"))
+                                                                          .findAny().orElseThrow(() -> new AssertionError("Olt commissioning processId wasn't found"));
 
                     assertThat(processLog.getProcessStatus()).isEqualTo(ProcessStatus.COMPLETED);
                     assertThat(processLog.getProcessResolutionStatus()).isEqualTo(ResolutionStatus.SUCCESSFUL);
@@ -173,21 +173,21 @@ public class OltCommissioningRobot {
         long accessLinesPerPort = getPreProvisioningAccessLineCount(olt);
 
         List<Device> deviceList = deviceResourceInventoryManagementClient.getClient().device().listDevice()
-                .endSzQuery(oltEndSz).depthQuery(3).executeAs(checkStatus(HTTP_CODE_OK_200));
+                                                                         .endSzQuery(oltEndSz).depthQuery(3).executeAs(checkStatus(HTTP_CODE_OK_200));
         assertEquals(deviceList.size(), 1L, "Device is not present");
         assertEquals(deviceList.get(0).getDeviceType(), DeviceType.OLT, "Device type is not OLT");
         assertEquals(deviceList.get(0).getEndSz(), oltEndSz, "Device EndSz mismatch");
         Device deviceAfterCommissioning = deviceList.get(0);
 
         List<Port> portList = deviceResourceInventoryManagementClient.getClient().port().listPort()
-                .parentEquipmentRefEndSzQuery(oltEndSz).executeAs(checkStatus(HTTP_CODE_OK_200));
+                                                                     .parentEquipmentRefEndSzQuery(oltEndSz).executeAs(checkStatus(HTTP_CODE_OK_200));
 
         if (deviceList.get(0).getEmsNbiName().equals(EMS_NBI_NAME_SDX6320_16)) {
             assertEquals(portList.size(), olt.getNumberOfPonPorts() + olt.getNumberOfEthernetPorts(), "Ports number by Adtran mismatch");
             portsCount = olt.getNumberOfPonPorts();
         } else {
             portsCount = portList.stream()
-                    .filter(port -> port.getPortType().equals(PortType.PON)).count();
+                                 .filter(port -> port.getPortType().equals(PortType.PON)).count();
         }
 
         // check device lifecycle state
@@ -196,50 +196,50 @@ public class OltCommissioningRobot {
         // check uplink port lifecycle state
         if (deviceList.get(0).getEmsNbiName().equals(EMS_NBI_NAME_SDX6320_16)) {
             Optional<Port> uplinkPort = portList.stream()
-                    .filter(port -> port.getPortName().equals(olt.getOltPort()))
-                    .filter(port -> port.getPortType().equals(PortType.ETHERNET))
-                    .findFirst();
+                                                .filter(port -> port.getPortName().equals(olt.getOltPort()))
+                                                .filter(port -> port.getPortType().equals(PortType.ETHERNET))
+                                                .findFirst();
             assertTrue(uplinkPort.isPresent(), "ADTRAN No uplink port is present");
             assertEquals(uplinkPort.get().getLifeCycleState(), LifeCycleState.OPERATING, "Uplink port state after commissioning is not in operating state");
         } else {
             Optional<Port> uplinkPort = portList.stream()
-                    .filter(port -> port.getParentEquipmentRef().getSlotName().equals(olt.getOltSlot()))
-                    .filter(port -> port.getPortName().equals(olt.getOltPort()))
-                    .filter(port -> port.getPortType().equals(PortType.ETHERNET))
-                    .findFirst();
+                                                .filter(port -> port.getParentEquipmentRef().getSlotName().equals(olt.getOltSlot()))
+                                                .filter(port -> port.getPortName().equals(olt.getOltPort()))
+                                                .filter(port -> port.getPortType().equals(PortType.ETHERNET))
+                                                .findFirst();
             assertTrue(uplinkPort.isPresent(), "HUAWEI No uplink port is present");
             assertEquals(uplinkPort.get().getLifeCycleState(), LifeCycleState.OPERATING, "Uplink port state after commissioning is not in operating state");
         }
 
         // check pon ports lifecycle state
         boolean allPortsInOperatingState = portList.stream()
-                .filter(port -> port.getPortType().equals(PortType.PON))
-                .map(Port::getLifeCycleState).allMatch(LifeCycleState.OPERATING::equals);
+                                                   .filter(port -> port.getPortType().equals(PortType.PON))
+                                                   .map(Port::getLifeCycleState).allMatch(LifeCycleState.OPERATING::equals);
         assertTrue(allPortsInOperatingState, "Some port is in not OPERATING state");
 
         List<AccessLineDto> wgAccessLines = accessLineResourceInventoryClient.getClient().accessLineController().searchAccessLines()
-                .body(new SearchAccessLineDto().endSz(oltEndSz)).executeAs(checkStatus(HTTP_CODE_OK_200))
-                .stream().filter(accessLineDto -> accessLineDto.getStatus().equals(AccessLineStatus.WALLED_GARDEN)).collect(Collectors.toList());
+                                                                             .body(new SearchAccessLineDto().endSz(oltEndSz)).executeAs(checkStatus(HTTP_CODE_OK_200))
+                                                                             .stream().filter(accessLineDto -> accessLineDto.getStatus().equals(AccessLineStatus.WALLED_GARDEN)).collect(Collectors.toList());
         long wgLinesCount = wgAccessLines.size();
 
         assertEquals(wgLinesCount, portsCount * accessLinesPerPort, "wgLinesCount mismatch");
 
         List<Integer> anpTagsList = wgAccessLines.stream().map(accessLineDto -> accessLineDto.getAnpTag().getAnpTag())
-                .filter(anpTagValue -> anpTagValue >= 128).collect(Collectors.toList());
+                                                 .filter(anpTagValue -> anpTagValue >= 128).collect(Collectors.toList());
 
         assertEquals(anpTagsList.size(), portsCount * accessLinesPerPort, "anpTagsList size mismatch");
 
         assertTrue(anpTagsList.contains(128), "anpTagsList contains mismatch");
 
         long homeIdCount = accessLineResourceInventoryClient.getClient().homeIdController().searchHomeIds()
-                .body(new SearchHomeIdDto().endSz(oltEndSz)).executeAs(checkStatus(HTTP_CODE_OK_200))
-                .stream().filter(homeIdDto -> homeIdDto.getStatus().equals(HomeIdStatus.FREE)).count();
+                                                            .body(new SearchHomeIdDto().endSz(oltEndSz)).executeAs(checkStatus(HTTP_CODE_OK_200))
+                                                            .stream().filter(homeIdDto -> homeIdDto.getStatus().equals(HomeIdStatus.FREE)).count();
 
         assertEquals(homeIdCount, portsCount * HOME_ID_POOL_PER_PORT, "HomeIdCount mismatch");
 
         long backhaulIdCount = accessLineResourceInventoryClient.getClient().backhaulIdController().searchBackhaulIds()
-                .body(new SearchBackhaulIdDto().endSz(oltEndSz)).executeAs(checkStatus(HTTP_CODE_OK_200))
-                .stream().filter(backhaulIdDto -> BackhaulStatus.CONFIGURED.equals(backhaulIdDto.getStatus())).count();
+                                                                .body(new SearchBackhaulIdDto().endSz(oltEndSz)).executeAs(checkStatus(HTTP_CODE_OK_200))
+                                                                .stream().filter(backhaulIdDto -> BackhaulStatus.CONFIGURED.equals(backhaulIdDto.getStatus())).count();
 
         assertEquals(backhaulIdCount, portsCount, "backhaulIdCount mismatched with portsCount");
     }
@@ -257,7 +257,7 @@ public class OltCommissioningRobot {
 
         // check uplink state
         List<Uplink> uplinkList = deviceResourceInventoryManagementClient.getClient().uplink().listUplink()
-                .portsEquipmentBusinessRefEndSzQuery(oltDevice.getEndsz()).executeAs(checkStatus(HTTP_CODE_OK_200));
+                                                                         .portsEquipmentBusinessRefEndSzQuery(oltDevice.getEndsz()).executeAs(checkStatus(HTTP_CODE_OK_200));
         assertEquals(uplinkList.size(), 1L, "uplinkList.size missmatch");
         assertEquals(uplinkList.get(0).getState(), UplinkState.ACTIVE, "UplinkState is not active");
 
@@ -285,7 +285,7 @@ public class OltCommissioningRobot {
 
         // check ANCP Session
         List<AncpSession> ancpSessionList = deviceResourceInventoryManagementClient.getClient().ancpSession().listAncpSession()
-                .accessNodeEquipmentBusinessRefEndSzQuery(oltDevice.getEndsz()).executeAs(checkStatus(HTTP_CODE_OK_200));
+                                                                                   .accessNodeEquipmentBusinessRefEndSzQuery(oltDevice.getEndsz()).executeAs(checkStatus(HTTP_CODE_OK_200));
         assertEquals(ancpSessionList.size(), 1L, "ancpSessionList.size missmatch");
         assertEquals(ancpSessionList.get(0).getConfigurationStatus(), "ACTIVE", "ANCP ConfigurationStatus missmatch");
 
@@ -294,7 +294,7 @@ public class OltCommissioningRobot {
     @Step("Restore OSR Database state")
     public void restoreOsrDbState() {
         accessLineResourceInventoryFillDbClient.getClient().fillDatabase().deleteDatabase()
-                .execute(checkStatus(HTTP_CODE_OK_200));
+                                               .execute(checkStatus(HTTP_CODE_OK_200));
         oltDiscoveryClient.reset();
     }
 
@@ -302,7 +302,7 @@ public class OltCommissioningRobot {
     public void clearResourceInventoryDataBase(OltDevice oltDevice) {
         String endSz = oltDevice.getEndsz();
         deviceTestDataManagementClient.getClient().deviceTestDataManagement().deleteTestData().deviceEndSzQuery(endSz)
-                .execute(checkStatus(HTTP_CODE_NO_CONTENT_204));
+                                      .execute(checkStatus(HTTP_CODE_NO_CONTENT_204));
     }
 
     @Step("enable unleash feature toggle: service.olt-resource-inventory-ui.uplink-import")
@@ -313,8 +313,8 @@ public class OltCommissioningRobot {
     @Step(" get the count of pre-provisioning access lines from olt-material-catalog")
     public Integer getPreProvisioningAccessLineCount(OltDevice oltDevice) {
         DeviceTemplate deviceTemplate = oltMaterialCatalogClient.getClient().deviceTemplateController()
-                .findDeviceTemplateByEmsNbiName().emsnbinamePath(oltDevice.getBezeichnung())
-                .executeAs(checkStatus(HTTP_CODE_OK_200));
+                                                                .findDeviceTemplateByEmsNbiName().emsnbinamePath(oltDevice.getBezeichnung())
+                                                                .executeAs(checkStatus(HTTP_CODE_OK_200));
 
         String str = deviceTemplate.getPreprovisioningStrategy();
         log.info("getPreprovisioningStrategy {} for emsNbiName = {}", str, oltDevice.getBezeichnung());
