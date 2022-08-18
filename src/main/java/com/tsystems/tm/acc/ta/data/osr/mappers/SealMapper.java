964e863b21ce95c8a2f06cd3e731596616538ba4
Lutz Schaefer <lutz.schaefer@t-systems.com>
2022-08-18 15:25:10 +0200

    DIGIHUB-133353  update SEAL model for dynamic OltDevice Stub mapping to SEAL REST API version 1.11.0

--- src/main/java/com/tsystems/tm/acc/ta/data/osr/mappers/SealMapper.java
+++ src/main/java/com/tsystems/tm/acc/ta/data/osr/mappers/SealMapper.java
@@ -1,11 +1,16 @@
 package com.tsystems.tm.acc.ta.data.osr.mappers;
 
 import com.tsystems.tm.acc.ta.data.osr.models.OltDevice;
-import com.tsystems.tm.acc.tests.osr.seal.client.model.*;
 import com.tsystems.tm.acc.tests.osr.seal.external.v1_2_01.client.model.*;
 import com.tsystems.tm.acc.tests.osr.seal.external.v1_7_1.client.model.CallbackV1OntPonDetectionRequest;
 import com.tsystems.tm.acc.tests.osr.seal.external.v1_7_1.client.model.Callbackv1ontpondetectionrequestEvents;
 import com.tsystems.tm.api.client.seal.external.model.CallbackV1OltOltBasicConfigurationTaskRequest;
+import com.tsystems.tm.api.client.seal.external.model.CallbackV1GetAccessnodeInventoryRequest;
+import com.tsystems.tm.api.client.seal.external.model.CallbackV1GetAccessnodeInventoryRequestPayload;
+import com.tsystems.tm.api.client.seal.external.model.CallbackV1GetAccessnodeInventoryRequestPayloadManagedElement;
+import com.tsystems.tm.api.client.seal.external.model.CallbackV1GetAccessnodeInventoryRequestPayloadModules;
+import com.tsystems.tm.api.client.seal.external.model.CallbackV1GetAccessnodeInventoryRequestPayloadPorts;
+
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -90,66 +95,66 @@ public class SealMapper<CallbackGetEmptyListOfEmsEventsRequest> {
         }
     }
 
-    public CallbackGetAccessnodeInventoryRequest getCallbackGetAccessnodeInventoryRequest(OltDevice olt) {
+    public CallbackV1GetAccessnodeInventoryRequest getCallbackGetAccessnodeInventoryRequest(OltDevice olt) {
         if (olt.getBezeichnung().equals("SDX 6320-16")) {
             return getCallbackGetAccessnodeInventoryRequestAdtran(olt);
         }
         return getCallbackGetAccessnodeInventoryRequestMA5600(olt);
     }
 
-    public CallbackGetAccessnodeInventoryRequest getCallbackGetAccessnodeInventoryRequestMA5600(OltDevice olt) {
-        return new CallbackGetAccessnodeInventoryRequest()
-                .payload(new CallbackgetaccessnodeinventoryrequestPayload()
-                        .managedElement(new CallbackgetaccessnodeinventoryrequestPayloadManagedElement()
+    public CallbackV1GetAccessnodeInventoryRequest getCallbackGetAccessnodeInventoryRequestMA5600(OltDevice olt) {
+        return new CallbackV1GetAccessnodeInventoryRequest()
+                .payload(new CallbackV1GetAccessnodeInventoryRequestPayload()
+                        .managedElement(new CallbackV1GetAccessnodeInventoryRequestPayloadManagedElement()
                                 .name(olt.getEndsz().replace('/', '_'))
                                 .ipAddress(olt.getIpAdresse())
                                 .productName(olt.getBezeichnung())
                                 .manufacturer(olt.getHersteller())
                                 .softwareVersion(olt.getFirmwareVersion())
-                                .resourceState(CallbackgetaccessnodeinventoryrequestPayloadManagedElement.ResourceStateEnum.WORKING)
-                                .communicationState(CallbackgetaccessnodeinventoryrequestPayloadManagedElement.CommunicationStateEnum.AVAILABLE)
+                                .resourceState(CallbackV1GetAccessnodeInventoryRequestPayloadManagedElement.ResourceStateEnum.WORKING)
+                                .communicationState(CallbackV1GetAccessnodeInventoryRequestPayloadManagedElement.CommunicationStateEnum.CS_AVAILABLE)
                         )
                         .modules(Stream.concat(
                                 IntStream.range(START_PON_SLOT, START_PON_SLOT + olt.getNumberOfPonSlots())
-                                        .mapToObj(slot -> new CallbackgetaccessnodeinventoryrequestPayloadModules()
+                                        .mapToObj(slot -> new CallbackV1GetAccessnodeInventoryRequestPayloadModules()
                                                 .slot(String.valueOf(slot))
                                                 .manufacturer(olt.getHersteller())
                                                 .shelf(DEFAULT_SHELF)
                                                 .installedEquipmentObjectType("H805GPBD")
                                                 .installedVersion("507(2015-8-27)")
                                                 .installedSerialNumber("121BQW10B6123" + String.format("%03d", moduleCount.getAndIncrement()))
-                                                .resourceState(CallbackgetaccessnodeinventoryrequestPayloadModules.ResourceStateEnum.INSTALLING_INSTALLED)
-                                                .resourceFulfillmentState(CallbackgetaccessnodeinventoryrequestPayloadModules.ResourceFulfillmentStateEnum.IN_SERVICE)),
+                                                .resourceState("INSTALLING_INSTALLED")
+                                                .resourceFulfillmentState("IN_SERVICE")),
                                 IntStream.range(START_ETHERNET_SLOT, START_ETHERNET_SLOT + olt.getNumberOfEthernetSlots())
-                                        .mapToObj(slot -> new CallbackgetaccessnodeinventoryrequestPayloadModules()
+                                        .mapToObj(slot -> new CallbackV1GetAccessnodeInventoryRequestPayloadModules()
                                                 .slot(String.valueOf(slot))
                                                 .manufacturer(olt.getHersteller())
                                                 .shelf(DEFAULT_SHELF)
                                                 .installedEquipmentObjectType("H801X2CS")
                                                 .installedVersion("507(2019-07-04)")
                                                 .installedSerialNumber("H801X2715258001" + String.format("%03d", moduleCount.getAndIncrement()))
-                                                .resourceState(CallbackgetaccessnodeinventoryrequestPayloadModules.ResourceStateEnum.INSTALLING_INSTALLED)
-                                                .resourceFulfillmentState(CallbackgetaccessnodeinventoryrequestPayloadModules.ResourceFulfillmentStateEnum.IN_SERVICE))
+                                                .resourceState("INSTALLING_INSTALLED")
+                                                .resourceFulfillmentState("IN_SERVICE"))
                         ).collect(Collectors.toList()))
                         .ports(
                                 Stream.of(
                                                 IntStream.range(START_PON_SLOT, START_PON_SLOT + olt.getNumberOfPonSlots())
                                                         .mapToObj(slot -> IntStream.range(0, PON_PORTS_NUMBER)
-                                                                .mapToObj(i -> new CallbackgetaccessnodeinventoryrequestPayloadPorts()
+                                                                .mapToObj(i -> new CallbackV1GetAccessnodeInventoryRequestPayloadPorts()
                                                                         .installedMatNumberSFP(OPTIC_VENDOR_MAT_NUMBER)
                                                                         .installedPartNumberSFP(OPTIC_VENDOR_PART_NUMBER)
                                                                         .port(String.valueOf(i))
-                                                                        .portType(CallbackgetaccessnodeinventoryrequestPayloadPorts.PortTypeEnum.PON)
+                                                                        .portType(CallbackV1GetAccessnodeInventoryRequestPayloadPorts.PortTypeEnum.PON)
                                                                         .shelf(DEFAULT_SHELF)
                                                                         .slot(String.valueOf(slot))
                                                                 )),
                                                 IntStream.range(START_ETHERNET_SLOT, START_ETHERNET_SLOT + olt.getNumberOfEthernetSlots())
                                                         .mapToObj(slot -> IntStream.range(0, ETH_PORTS_NUMBER)
-                                                                .mapToObj(i -> new CallbackgetaccessnodeinventoryrequestPayloadPorts()
+                                                                .mapToObj(i -> new CallbackV1GetAccessnodeInventoryRequestPayloadPorts()
                                                                         .installedMatNumberSFP(OPTIC_VENDOR_MAT_NUMBER)
                                                                         .installedPartNumberSFP(OPTIC_VENDOR_PART_NUMBER)
                                                                         .port(String.valueOf(i))
-                                                                        .portType(CallbackgetaccessnodeinventoryrequestPayloadPorts.PortTypeEnum.ETHERNET)
+                                                                        .portType(CallbackV1GetAccessnodeInventoryRequestPayloadPorts.PortTypeEnum.ETHERNET)
                                                                         .shelf(DEFAULT_SHELF)
                                                                         .slot(String.valueOf(slot))
                                                                 ))
@@ -162,33 +167,33 @@ public class SealMapper<CallbackGetEmptyListOfEmsEventsRequest> {
 
     }
 
-    public CallbackGetAccessnodeInventoryRequest getCallbackGetAccessnodeInventoryRequestAdtran(OltDevice olt) {
-        return new CallbackGetAccessnodeInventoryRequest()
-                .payload(new CallbackgetaccessnodeinventoryrequestPayload()
-                        .managedElement(new CallbackgetaccessnodeinventoryrequestPayloadManagedElement()
+    public CallbackV1GetAccessnodeInventoryRequest getCallbackGetAccessnodeInventoryRequestAdtran(OltDevice olt) {
+        return new CallbackV1GetAccessnodeInventoryRequest()
+                .payload(new CallbackV1GetAccessnodeInventoryRequestPayload()
+                        .managedElement(new CallbackV1GetAccessnodeInventoryRequestPayloadManagedElement()
                                 .name(olt.getEndsz().replace('/', '_'))
                                 .ipAddress(olt.getIpAdresse())
                                 .productName(olt.getBezeichnung())
                                 .manufacturer(olt.getHersteller())
                                 .softwareVersion(olt.getFirmwareVersion())
-                                .resourceState(CallbackgetaccessnodeinventoryrequestPayloadManagedElement.ResourceStateEnum.WORKING)
-                                .communicationState(CallbackgetaccessnodeinventoryrequestPayloadManagedElement.CommunicationStateEnum.AVAILABLE)
+                                .resourceState(CallbackV1GetAccessnodeInventoryRequestPayloadManagedElement.ResourceStateEnum.WORKING)
+                                .communicationState(CallbackV1GetAccessnodeInventoryRequestPayloadManagedElement.CommunicationStateEnum.CS_AVAILABLE)
                         )
                         .ports(
                                 Stream.of(
                                                 IntStream.range(SDX_START_PON_PORT, olt.getNumberOfPonPorts() + SDX_START_PON_PORT)
-                                                        .mapToObj(i -> new CallbackgetaccessnodeinventoryrequestPayloadPorts()
+                                                        .mapToObj(i -> new CallbackV1GetAccessnodeInventoryRequestPayloadPorts()
                                                                 .installedMatNumberSFP(OPTIC_VENDOR_MAT_NUMBER)
                                                                 .installedPartNumberSFP(OPTIC_VENDOR_PART_NUMBER)
                                                                 .port(String.valueOf(i))
-                                                                .portType(CallbackgetaccessnodeinventoryrequestPayloadPorts.PortTypeEnum.PON)
+                                                                .portType(CallbackV1GetAccessnodeInventoryRequestPayloadPorts.PortTypeEnum.PON)
                                                                 .shelf(DEFAULT_SHELF)),
                                                 IntStream.range(SDX_START_ETHERNET_PORT, olt.getNumberOfEthernetPorts() + SDX_START_ETHERNET_PORT)
-                                                        .mapToObj(i -> new CallbackgetaccessnodeinventoryrequestPayloadPorts()
+                                                        .mapToObj(i -> new CallbackV1GetAccessnodeInventoryRequestPayloadPorts()
                                                                 .installedMatNumberSFP("")
                                                                 .installedPartNumberSFP("")
                                                                 .port(String.valueOf(i))
-                                                                .portType(CallbackgetaccessnodeinventoryrequestPayloadPorts.PortTypeEnum.ETHERNET)
+                                                                .portType(CallbackV1GetAccessnodeInventoryRequestPayloadPorts.PortTypeEnum.ETHERNET)
                                                                 .shelf(DEFAULT_SHELF))
 
                                         )
